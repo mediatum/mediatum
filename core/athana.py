b0b1042e589563086f8ca7fe849ca6a5aac66ba1
kramm <kramm>
2007-09-05 16:06:28 +0000

    fixed root dir issue

--- core/athana.py
+++ core/athana.py
@@ -32,7 +32,7 @@
 Parse HTML and compile to TALInterpreter intermediate code.
 """
 
-RCS_ID =  '$Id: athana.py,v 1.4 2007/09/05 15:51:10 kramm Exp $'
+RCS_ID =  '$Id: athana.py,v 1.5 2007/09/05 16:06:28 kramm Exp $'
 
 import sys
 
@@ -5789,7 +5789,7 @@ HTTP_INSUFFICIENT_STORAGE         = 507
 HTTP_NOT_EXTENDED                 = 510
 
 GLOBAL_TEMP_DIR="/tmp/"
-GLOBAL_ROOT_DIR="/"
+GLOBAL_ROOT_DIR="no-root-dir-set"
 verbose = 0
 multithreading_enabled = 0
 number_of_threads = 32
@@ -5859,6 +5859,8 @@ class WebContext:
     def _make_inifiles(self, root, path):
         dirs = path.split("/")
         path = root
+        for f in self.files:
+            print f.filename
         for dir in dirs:
             path = join_paths(path, dir)
             inifile = join_paths(path, "__init__.py")
@@ -5913,6 +5915,7 @@ class WebContext:
         self.id_to_function = {}
         for file in self.files:
             filename = file.getFileName()
+            lg.log("loading module "+filename)
             m = self._load_module(filename)
             
             global macroresolvers,translators,ftphandlers
@@ -6672,9 +6675,11 @@ class zip_filesystem:
 
 
 def setBase(base):
+    global GLOBAL_ROOT_DIR
     GLOBAL_ROOT_DIR = qualify_path(base)
    
 def setTempDir(tempdir):
+    global GLOBAL_TEMP_DIR
     GLOBAL_TEMP_DIR = qualify_path(tempdir)
     
 def addMacroResolver(m):

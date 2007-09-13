39b1db0249c8b579fb43da90bd79c8f472eadfa0
seiferta <seiferta>
2007-09-13 11:20:09 +0000

    flush operation added

---
+++ web/admin/modules/flush.py
@@ -0,0 +1,57 @@
+"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2007 Arne Seifert <seiferta@in.tum.de>
+ Copyright (C) 2007 Matthias Kramm <kramm@in.tum.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import core.tree as tree
+import core.webconfig as webconfig
+from web.admin.adminutils import getAdminStdVars
+
+def validate(req, op):
+
+    if "op" in req.params.keys():
+        if req.params.get("op")=="flushall":
+            webconfig.flush(req)
+            req.writeTAL("web/admin/modules/flush.html", {}, macro="flushed")
+            return ""
+    else:
+        for key in req.params.keys():
+        
+            if key.startswith("flush_db"):
+                print "flush db"
+                tree.flush()
+                op = "db"
+                return view(req, op)
+                
+            if key.startswith("flush_all"):
+                print "flush all"
+                op = "all"
+                return view(req, op)
+    return view(req, op)
+
+
+def view(req, op):
+    v = getAdminStdVars(req)
+    
+    v["msg"] = ""
+    v["op"] = op
+    if op == "db":
+        v["msg"] = "admin_flush_data_cleared" 
+    elif op == "all":
+        v["msg"] = "admin_flush_all_cleared" 
+    return req.getTAL("web/admin/modules/flush.html", v, macro="view")
+    

4c48204c46c126a4db5ef085fdae104e1d5c41c8
seiferta <seiferta>
2007-10-16 11:22:14 +0000

    search indexer updated

--- core/search/dbindexer.py
+++ core/search/dbindexer.py
@@ -19,6 +19,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import re
+import os
 import core.startup
 import core.config as config
 import sqlite3 as sqlite
@@ -48,9 +50,10 @@ class DbIndexer:
         try:
             # simple search table
             self.cur.execute('CREATE VIRTUAL TABLE fullsearchmeta USING fts2(id, type, scheme, value)')
-            
             # extended search table
             self.cur.execute('CREATE VIRTUAL TABLE searchmeta USING fts2(id, type, scheme, fieldnames, '+s+')')
+            # fulltext search table
+            self.cur.execute('CREATE VIRTUAL TABLE textsearchmeta USING fts2(id, type, scheme, value)')
             
         except sqlite.OperationalError:
             print "searchdatabase already initialised"
@@ -59,7 +62,8 @@ class DbIndexer:
     def clearIndex(self):
         print "\nclearing index tables..."
         for table in ["fullsearchmeta", "fullsearchmeta_content", "fullsearchmeta_segdir", "fullsearchmeta_segments", 
-                      "searchmeta", "searchmeta_content", "searchmeta_segdir", "searchmeta_segments"]:
+                      "searchmeta", "searchmeta_content", "searchmeta_segdir", "searchmeta_segments",
+                      "textsearchmeta", "textsearchmeta_content", "textsearchmeta_segdir", "textsearchmeta_segments"]:
             try:
                 self.cur.execute("delete from "+ table)
             except:
@@ -70,17 +74,15 @@ class DbIndexer:
     def dropIndex(self):
         print "\ndropping index tables..."
         for table in ["fullsearchmeta", "fullsearchmeta_content", "fullsearchmeta_segdir", "fullsearchmeta_segments", 
-                      "searchmeta", "searchmeta_content", "searchmeta_segdir", "searchmeta_segments"]:
+                      "searchmeta", "searchmeta_content", "searchmeta_segdir", "searchmeta_segments",
+                      "textsearchmeta", "textsearchmeta_content", "textsearchmeta_segdir", "textsearchmeta_segments"
+                      ]:
             try:
                 self.cur.execute("drop table "+table)
             except sqlite.OperationalError:
                 print " - table", table, "not found"
         self.db.commit()
-        print "...dropped"
-
-    def indexNode(self, node):
-        None
-        
+        print "...dropped"     
         
         
     def nodeToSimpleSearch(self, node):
@@ -88,8 +90,8 @@ class DbIndexer:
         try:
             sql = 'INSERT INTO fullsearchmeta (id, type, scheme, value) VALUES("'+ str(node.id)+'", "'+node.getType().type+'", "'+node.getType().metadatatypename+'", "'+ str(node.name) + '| '
             # attributes
-            for attr in node.items():
-                sql += str(esc(iso2utf8(attr[1])))+'| '
+            for key,value in node.items():
+                sql += str(esc(iso2utf8(value)))+'| '
 
             # files
             for file in node.getFiles():
@@ -131,13 +133,58 @@ class DbIndexer:
             return True
         except:
             return False
-    
-    
+            
+    """
+        mode values:
+            0: index fulltext without changes
+            1: optimize fulltext (each word once)
+            2: each word once with number of occurences
+    """
+    def nodeToFulltextSearch(self, node, mode=0):
+        # build fulltext index from node
+        
+        for file in node.getFiles():
+            if file.getType() == "fulltext" and os.path.exists(file.getPath()):
+                data = {}
+                content = ''
+                f = open(file.getPath())
+                try:
+                    for line in f:
+                        if mode==0:
+                            content += line
+                        else:
+                            for w in re.findall(r, line):
+                                if not w in data.keys():
+                                    data[w] = 1
+                            try:
+                                data[w] += 1
+                            except KeyError:
+                                data[w] = 1
+                finally:
+                    f.close()
+
+                if mode==1:
+                    for key in data.keys():
+                        content += key + " "
+                elif mode==2:
+                    for key in data.keys():
+                        content += key + " ["+ str(data[key])+"] "
+
+                if len(content)>0:
+                    try:
+                        sql = 'INSERT INTO textsearchmeta (id, type, scheme, value) VALUES("'+str(node.id)+'", "'+str(node.getType().type)+'", "'+str(node.getType().metadatatypename)+'", "'+iso2utf8(esc(content))+'")'
+                        self.cur.execute(sql)
+                    except:
+                        return False
+                else:
+                    print "no Content"
+        return True
     
         
     def runIndexer(self):
         errorindexfull = []
         errorindexext = []
+        errorindextext = []
         
         root = tree.getRoot()
         self.clearIndex()
@@ -151,10 +198,12 @@ class DbIndexer:
             if not self.nodeToExtSearch(node):
                 errorindexext.append(node.id)
  
+            if not self.nodeToFulltextSearch(node):
+                errorindextext.append(node.id)              
             nodes+=1
         
         self.db.commit()
-        print "  import of", nodes, "nodes finished with",len(errorindexfull)+len(errorindexext), "error(s)."
+        print "  import of", nodes, "nodes finished with",len(errorindexfull)+len(errorindexext)+len(errorindextext), "error(s)."
         if len(errorindexfull)>0:
             print "   Simple Search Index:\n    error(s) in node with id:"
             for item in errorindexfull:
@@ -164,8 +213,24 @@ class DbIndexer:
             print "   Extended Search Index:\n    error(s) in node with id:"
             for item in errorindexext:
                 print "    -", item
+
+        if len(errorindextext)>0:
+            print "   Fulltext Search Index:\n    error(s) in node with id:"
+            for item in errorindextext:
+                print "    -", item
                 
-id = DbIndexer()
-id.runIndexer()
-#id.dropIndex()
+    
+    def runQuery(self):
+        #sql = "select id, field4 from searchmeta where type='image' and scheme='lt' and field4 < '2007-01-01T00:00:00' order by field4 desc"
+        sql = "select id, field9 from searchmeta where type='document' and scheme='diss' and field9 < '2000' order by field9 desc"
+        res = self.cur.execute(sql)
+        for item in res.fetchall():
+            print item
+        
+    
+    
+#id = DbIndexer()
+#id.runIndexer() # create index
+#id.dropIndex()  #  drop tables
+#id.runQuery() # run different queries
 

d148905ee0f9de58ff2f4be13068c33f53744da5
seiferta <seiferta>
2007-11-08 13:34:02 +0000

    import fixed

--- web/edit/edit_license.py
+++ web/edit/edit_license.py
@@ -18,8 +18,6 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import core.tree as tree
-#from utils import *
-#from date import *
 from core.acl import AccessData
 
 def edit_license(req, ids):

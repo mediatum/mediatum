7afade3fc62982e472fd007bf5a6309dd583023f
seiferta <seiferta>
2008-04-07 13:36:18 +0000

    classes for nodetype mapping added

---
+++ schema/mapping.py
@@ -0,0 +1,186 @@
+"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2007 Arne Seifert <seiferta@in.tum.de>
+ Copyright (C) 2007 Matthias Kramm <kramm@in.tum.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import core.tree as tree
+
+
+def getMappings():
+    try:
+        mappings = tree.getRoot("mappings")
+    except tree.NoSuchNodeError,e:
+        root = tree.getRoot()
+        root.addChild(tree.Node("mappings", "mappings"))
+        mappings = tree.getRoot("mappings")
+    
+    try:
+        return mappings.getChildren()
+    except tree.NoSuchNodeError,e:
+        return []
+
+
+def getMapping(id):
+    try:
+        return tree.getNode(id)
+    except tree.NoSuchNodeError,e:
+        mappings = tree.getRoot("mappings")
+        return mappings.getChild(id)
+
+
+def updateMapping(name, namespace="", namespaceurl="", description="", header="", footer="", separator="", standardformat="", id=0):
+    if id!="" and int(id)>0:
+        mapping = tree.getNode(id)
+    else:
+        mappings = tree.getRoot("mappings")
+        mapping = tree.Node(name=name, type="mapping")
+        mappings.addChild(mapping)
+    mapping.setName(name)
+    mapping.setDescription(description)
+    mapping.setNamespace(namespace)
+    mapping.setNamespaceUrl(namespaceurl)
+    mapping.setHeader(header)
+    mapping.setFooter(footer)
+    mapping.setSeparator(separator)
+    mapping.setStandardFormat(standardformat)
+
+def deleteMapping(name):
+    mappings = tree.getRoot("mappings")
+    mappings.removeChild(getMapping(name))
+    
+    
+def updateMappingField(parentid, name, description="", exportformat="", mandatory=False, id=0):
+    mapping = tree.getNode(parentid)
+    if id!="" and int(id)>0:
+        mappingfield = tree.getNode(id)
+    else:
+        mappingfield = tree.Node(name=name, type="mappingfield")
+        mapping.addChild(mappingfield)
+    mappingfield.setName(name)
+    mappingfield.setDescription(description)
+    mappingfield.setExportFormat(exportformat)
+    mappingfield.setMandatory(mandatory)
+    
+
+def deleteMappingField(name):
+    node = tree.getNode(name)
+    for p in node.getParents():
+        if p.type=="mapping":
+            p.removeChild(node)
+            return
+            
+
+class Mapping(tree.Node):
+
+    def getName(self):
+        return self.get("name")
+    def setName(self, n):
+        self.set("name", n)
+
+    def getDescription(self):
+        return self.get("description")
+    def setDescription(self, description):
+        self.set("description", description)
+        
+    def getNamespace(self):
+        return self.get("namespace")
+    def setNamespace(self, namespace):
+        self.set("namespace", namespace)
+        
+    def getNamespaceUrl(self):
+        return self.get("namespaceurl")
+    def setNamespaceUrl(self, url):
+        self.set("namespaceurl", url)
+        
+    def getHeader(self):
+        return self.get("header")
+    def setHeader(self, header):
+        self.set("header", header)
+        
+    def getFooter(self):
+        return self.get("footer")
+    def setFooter(self, footer):
+        self.set("footer", footer)
+        
+    def getSeparator(self):
+        return self.get("separator")
+    def setSeparator(self, separator):
+        self.set("separator", separator)
+        
+    def getStandardFormat(self):
+        return self.get("standardformat")
+    def setStandardFormat(self, standardformat):
+        self.set("standardformat", standardformat)
+        
+    def getFields(self):
+        f = list(self.getChildren())
+        f.sort(lambda x, y: cmp(x.getName().lower(),y.getName().lower()))
+        return f
+        
+    def addField(self, field):
+        self.addChild(field)
+        
+    def getFieldtype(self):
+        return "mapping"
+    
+    def can_open(node):
+        return 0
+        
+        
+        
+class MappingField(tree.Node):
+    
+    def getName(self):
+        return self.get("name")
+    def setName(self, n):
+        self.set("name", n)
+
+    def getDescription(self):
+        return self.get("description")
+    def setDescription(self, description):
+        self.set("description", description)
+        
+    def getExportFormat(self):
+        return self.get("exportformat")
+    def setExportFormat(self, exportformat):
+        self.set("exportformat", exportformat)
+        
+    def getMandatory(self):
+        if self.get("mandatory")=="True":
+            return True
+        else:
+            return False
+    def setMandatory(self, mandatory):
+        if mandatory:
+            self.set("mandatory", "True")
+        else:
+            self.set("mandatory", "False")
+            
+    def getMapping(self):
+        for p in self.getParents():
+            try:
+                if p.getFieldtype()=="mapping":
+                    return p
+            except:
+                pass
+        return None
+    def getValues(self):
+        return ""
+        
+    def getFieldtype(self):
+        return "mappingfield"

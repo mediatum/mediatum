24e591f2aca2c96f726add8d0a260adcd6e03fe5
kramm <kramm>
2008-06-12 12:25:00 +0000

    fixed race conditions

--- gui/console.py
+++ gui/console.py
@@ -3,9 +3,34 @@ import os
 import thread
 import wx
 
+class LockedClass:
+    def __getattr__(self,name):
+        cls = self.__class__
+        if name in cls.__dict__:
+            a = cls.__dict__[name]
+        elif name in self.__dict__:
+            a = self.__dict__[name]
+        else:
+            raise AttributeError("no such member: "+str(name)) 
+
+        if a and type(a) == type(self.get):
+            lock = self.lock
+            def f(*args, **kargs):
+                lock.acquire()
+                try:
+                    return a()
+                finally:
+                    lock.release()
+            return f
+        return a
+
+
 class LogFrame(wx.Frame):
-    def __init__(self,application):
+    def __init__(self,application,lock):
         wx.Frame.__init__(self, None, -1, style = wx.DEFAULT_FRAME_STYLE)
+        self.lock = lock
+        self.output = []
+
         vsplit = wx.BoxSizer(wx.VERTICAL)
         
         #hsplit = wx.BoxSizer(wx.HORIZONTAL)
@@ -33,6 +58,15 @@ class LogFrame(wx.Frame):
         self.Bind(wx.EVT_CLOSE, self.exit)
 
     def OnPaint(self, event):
+        # copy content of self.output
+        self.lock.acquire()
+        try:
+            output,self.output = self.output[:],[]
+        finally:
+            self.lock.release()
+
+        for txt in output:
+            self.txt.WriteText(txt)
         self.Refresh()
 
     def exit(self, event):
@@ -41,25 +75,28 @@ class LogFrame(wx.Frame):
     def OpenBrowser(self, event):
         os.system("launch http://127.0.0.1:8081/")
 
-
 class MyApp(wx.App):
-    def __init__(self):
+    def __init__(self, lock):
         wx.App.__init__(self, redirect=False, filename=None, useBestVisual=False)
-        self.frame = LogFrame(self)
+        self.frame = LogFrame(self, lock)
+        self.lock = lock
         self.SetTopWindow(self.frame)
         self.frame.Show(True)
     def OnInit(self):
         return True
 
 class mystdout:
-    def __init__(self, stdout, app):
+    def __init__(self, stdout, app, lock):
         self.stdout = stdout
         self.app = app
-        self.lock = thread.allocate_lock()
+        self.lock = lock
     def write(self, s):
-        self.lock.acquire()
         try:
-            self.app.frame.txt.WriteText(s)
+            self.lock.acquire()
+            try:
+                self.app.frame.output += [s]
+            finally:
+                self.lock.release()
             wx.PostEvent(self.app.frame, wx.PaintEvent(42))
         except:
             self.stdout.write("error\n")
@@ -69,12 +106,11 @@ class mystdout:
                 self.stdout.write("  File \"%s\", line %d, in %s\n" % (l[0],l[1],l[2]))
                 self.stdout.write("    %s\n" % l[3])
             return s
-        finally:
-            self.lock.release()
     def flush(self):
         pass
 
 def start():
-    app = MyApp()
-    sys.stdout = sys.stderr = mystdout(sys.stdout, app)
+    lock = thread.allocate_lock()
+    app = MyApp(lock)
+    sys.stdout = sys.stderr = mystdout(sys.stdout, app, lock)
     thread_id = thread.start_new_thread(app.MainLoop, ())

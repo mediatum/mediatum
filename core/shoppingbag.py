5d987622cb228f39d0a4da4aa861f6cab0c15109
seiferta <seiferta>
2010-05-18 15:00:50 +0000

    shoppingbag class added

---
+++ core/shoppingbag.py
@@ -0,0 +1,48 @@
+﻿"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2010 Arne Seifert <seiferta@in.tum.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import core
+import core.tree as tree
+
+from utils.utils import mkKey
+
+
+class ShoppingBag(tree.Node):
+
+    def getItems(self):
+        return self.get("items").split(";")
+    def setItems(self, items=[]):
+        self.set("items", ";".join(items))
+
+    def isShared(self):
+        if self.get("key")!="":
+            return 1
+        return 0
+        
+    def getSharedKey(self):
+        return self.get("key")
+        
+    def createShareKey(self):
+        k = mkKey()
+        self.set("key", k)
+        return k
+        
+    def stopShare(self):
+        self.removeAttribute("key")
+        

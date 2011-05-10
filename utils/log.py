5997416deb526988079b1db64a73d63be3f8cbad
seiferta <seiferta>
2011-05-10 09:44:17 +0000

    log file handling updated for flexible path definition

--- utils/log.py
+++ utils/log.py
@@ -17,11 +17,14 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import os
 import core.config
 import logging
 import logging.handlers
 import sys
 import traceback
+
+LOGTYPES = ["database","backend","frontend","editor","usertracing","athana","errors","services","searchindex"]
 
 def initialize():
     log_screen = logging.getLogger('screen')
@@ -32,32 +35,47 @@ def initialize():
 
     log_everything = logging.getLogger('everything')
     log_everything.handlers = []
-    filename = core.config.get('logging.file.everything',None)
+
+    filename = core.config.get('logging.file.everything', None)
+    filepath = core.config.get('logging.path', None)
+    if not filename and not filepath:
+        filename = filepath = core.config.get('paths.tempdir')
+        filename += 'everything.log'
+        print 'Using temp directory (%s) for logging' %(filepath)
+    if not filename and filepath:
+        filename = filepath + 'everything.log'
+    if not filepath and filename:
+        filepath = filename[:filename.rfind("/")+1]
+    
     if filename:
+        if not os.path.exists(filename[:filename.rfind("/")+1]):
+            os.mkdir(filename[:filename.rfind("/")+1])
         l = logging.handlers.RotatingFileHandler(filename, maxBytes=33554432, backupCount=20)
         l.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
         log_everything.addHandler(l)
-    else:
-        print "No filename given for 'everything' log"
 
     log_everything.addHandler(log_screen)
     log_everything.setLevel(logging.DEBUG)
 
-    for name in ["database","backend","frontend","editor","usertracing","athana","errors","services","searchindex"]:
+    for name in LOGTYPES:
         log = logging.getLogger(name)
         log.handlers = []
         filename = core.config.get("logging.file."+name, None)
+        if not filename and filepath:
+            filename = filepath + name + '.log'
+
         if filename:
+            if not os.path.exists(filename[:filename.rfind("/")+1]):
+                os.mkdir(filename[:filename.rfind("/")+1])
             l = logging.handlers.RotatingFileHandler(filename, maxBytes=33554432, backupCount=20)
             l.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
             log.addHandler(l)
             log.addHandler(log_everything)
             log.setLevel(logging.DEBUG)
-        else:
-            print "No filename given for '%s' log" % name
 
-    logging.getLogger('backend').info('logging initialized')
+    logging.getLogger('backend').info('logging initialized (%s)' %(filepath))
 
+    
 def logException(message=None):
     errlog = logging.getLogger('errors')
     class Errorprint:

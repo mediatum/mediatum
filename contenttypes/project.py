49c1e76112120aa7725bd524191a218f17fb9b54
seiferta <seiferta>
2012-11-06 06:59:10 +0000

    project type added

---
+++ contenttypes/project.py
@@ -0,0 +1,30 @@
+"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2012 Arne Seifert <arne.seifert@tum.de>
+ Copyright (C) 2007 Matthias Kramm <kramm@in.tum.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from contenttypes.directory import Directory
+
+class Project(Directory):
+
+    def treeiconclass(node):
+        return "project"
+        
+    def getDefaultEditTab(node):
+        return "metadata"
+    

e43cc1222a78550792f331fe168aea1f4bc3d83c
wneudenberger <wneudenberger>
2013-04-23 08:16:30 +0000

    logging duration of creation of SortOrder dictionary

--- core/tree.py
+++ core/tree.py
@@ -274,6 +274,7 @@ def changed_metadata(node):
         last_changed_metadata_node = node.id
 
 def createSortOrder(field):
+    log.info("retrieving sort order for field "+field)
     t1 = time.time()
     reverse=0
     if field[0]=='-':
@@ -304,6 +305,9 @@ def createSortOrder(field):
             v = value
             i = i + 1
         id2pos[int(id)] = i
+    msg = "sort order retrieved, "+str(time.time()-t1)+" seconds  -  "
+    msg += "id2pos has %d keys" % len(id2pos)
+    log.info(msg)
     return id2pos
 
 class NodeList:

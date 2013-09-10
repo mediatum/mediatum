e37455d8afdea783fcb191eebb470699136e5620
Arne Seifert <arne.seifert@tum.de>
2013-09-10 16:14:31 +0200

    webservice for data upload

---
+++ web/services/upload/__init__.py
@@ -0,0 +1,105 @@
+"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2007 Arne Seifert <seiferta@in.tum.de>
+ Copyright (C) 2007 Matthias Kramm <kramm@in.tum.de>
+ Copyright (C) 2011 Werner Neudenberger <neudenberger@ub.tum.de>
+
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+import re
+import time
+import locale
+
+import logging
+
+import BaseHTTPServer
+response_code_dict = BaseHTTPServer.BaseHTTPRequestHandler.responses
+
+LOCALES = ["en_US.UTF-8", "english", "german"]
+
+try:
+    locale.setlocale(locale.LC_ALL, LOCALES[0]) # for thousands separator
+except:
+    pass
+
+try:
+    locale.setlocale(locale.LC_ALL, LOCALES[1]) # for thousands separator
+except: 
+    pass
+
+import handlers
+
+SERVICES_URL_HAS_HANDLER = 1
+SERVICES_URL_SIMPLE_REWRITE = 2
+
+urls = [
+        ["GET", "/index.html$", handlers.serve_file, ("/static/index.html", {}, {'filepath': 'index.html'}), SERVICES_URL_SIMPLE_REWRITE, None], 
+        ["GET", "/$",           handlers.serve_file, ("/static/index.html", {}, {'filepath': 'index.html'}), SERVICES_URL_SIMPLE_REWRITE, None], 
+        
+        ["POST", "/new$", handlers.upload_new_node, None, SERVICES_URL_HAS_HANDLER, None], 
+        ["POST", "/update/(?P<id>\d+)/{0,1}$", handlers.update_node, None, SERVICES_URL_HAS_HANDLER, None],  
+       ]
+
+DEBUG = True
+
+def request_handler(req):
+
+    handle_starttime = time.time()
+    matched = False
+    req_path = req.path
+    for method, pattern, handler_func, rewrite_target, url_flags, data in urls:
+        if method and method == req.command:
+            m = re.match(pattern, req_path)
+            if m:
+            
+                matched = True
+
+                if url_flags == SERVICES_URL_HAS_HANDLER:
+                    handle_path = req.path
+                    handle_params = req.params
+                    response_code, bytes_sent, d = handler_func(req, handle_path, handle_params, data, **m.groupdict())
+                    break
+                    
+                if url_flags == SERVICES_URL_SIMPLE_REWRITE:
+                    handle_path = rewrite_target[0]
+
+                    handle_params = req.params.copy()
+                    for key, value in rewrite_target[1].items(): 
+                        handle_params[key] = value    
+                        
+                    argsdict = m.groupdict().copy()
+                    for key, value in rewrite_target[2].items(): 
+                        argsdict[key] = value                    
+                                        
+                    response_code, bytes_sent, d = handler_func(req, handle_path, handle_params, data, **argsdict)
+                    break
+       
+    # try to call default handler, if no match
+    if not matched:
+        try:
+            if getattr(handlers, 'default_handler'):
+                response_code, bytes_sent = handlers.default_handler(req)
+        except:
+            response_code, bytes_sent = '404', 0
+                
+    
+    if not matched:
+        return req.error(404, "File "+req.path+" not found")
+    
+    return response_code 
+    
---
+++ web/services/upload/handlers.py
@@ -0,0 +1,266 @@
+"""
+ mediatum - a multimedia content repository
+
+ Copyright (C) 2007 Arne Seifert <seiferta@in.tum.de>
+ Copyright (C) 2007 Matthias Kramm <kramm@in.tum.de>
+ Copyright (C) 2011 Werner Neudenberger <neudenberger@ub.tum.de>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+import os
+import re
+import time
+import logging
+import base64
+
+import core.tree as tree
+import core.users as users
+import core.xmlnode as xmlnode
+
+from core import config
+from core.acl import AccessData
+from schema.schema import getMetaType, VIEW_DATA_ONLY
+
+from utils.date import format_date
+from utils.pathutils import getBrowsingPathList, isDescendantOf
+from utils.utils import u, u2, esc, intersection, getMimeType, OperationException
+from utils.fileutils import importFileFromData
+
+from core.schedules import delete_node_from_cache
+
+import web.services.jsonnode as jsonnode
+from web.services.rssnode import template_rss_channel, template_rss_item, feed_channel_dict, try_node_date
+
+if sys.version[0:3] < '2.6':
+    import simplejson as json
+else:
+    import json
+
+logger = logging.getLogger('services')
+host = "http://" + config.get("host.name")
+
+collections = tree.getRoot('collections')
+
+from web.services.cache import Cache
+from web.services.cache import date2string as cache_date2string
+import web.services.serviceutils as serviceutils
+
+FILTERCACHE_NODECOUNT_THRESHOLD = 2000000
+
+filtercache = Cache(maxcount=10, verbose=True)
+searchcache = Cache(maxcount=10, verbose=True)
+resultcache = Cache(maxcount=25, verbose=True)
+
+SEND_TIMETABLE = False
+
+
+    
+
+
+def upload_new_node(req, path, params, data):
+    
+    #get the user and verify the signature
+    if params.get('user'):
+        user=users.getUser(params.get('user'))
+        userAccess = AccessData(user=user)
+        
+        if userAccess.user != None:
+            valid = userAccess.verify_request_signature(req.fullpath,params)
+            if valid == False:
+                userAccess = None
+        else:
+            userAccess = None
+    else:
+        user=users.getUser('Gast')
+        userAccess = AccessData(user=user)
+    
+    parent = tree.getNode(params['parent'])
+    
+    #check user access
+    if userAccess != None and userAccess.hasAccess(parent, "write"):
+        pass
+    else:
+        s = "No Access"
+        req.write(s)
+        d = {}
+        d['status'] = 'fail'
+        d['html_response_code'] = '403' # forbidden (authorization will not help)
+        d['errormessage'] = 'no access'
+        return d['html_response_code'], len(s), d 
+    
+    
+    
+    datatype = params.get('type')
+    uploaddir = users.getUploadDir(user)
+    
+    n = tree.Node(name=params.get('name'), type=datatype)
+    data = base64.b64decode(params.get('data'))
+    file = importFileFromData('uploadTest.jpg',data)
+    n.addFile(file)
+    
+    metadata = json.loads(params.get('metadata'))
+    
+    #set provided metadata
+    for key,value in metadata.iteritems():
+        n.set(u(key),u(value))
+
+    # service flags
+    n.set("creator", user.getName())
+    n.set("creationtime", format_date())
+    
+    parent.addChild(n)
+    
+    #process the file, we've added to the new node
+    if hasattr(n,"event_files_changed"):
+        try:
+            n.event_files_changed()
+
+        except OperationException, e:
+            for file in n.getFiles():
+                if os.path.exists(file.retrieveFile()):
+                    os.remove(file.retrieveFile())
+            raise OperationException(e.value)
+    
+    #make sure the new node is visible immediately from the web service and the search index gets updated
+    n.setDirty()
+    delete_node_from_cache(parent)
+    
+    d = {}
+    d['status'] = 'Created'
+    d['html_response_code'] = '201' # ok
+    d['build_response_end'] = time.time()
+                   
+    s = "Created"
+     
+    #provide the uploader with the new node ID
+    req.reply_headers['NodeID'] = n.id
+    
+    #we need to write in case of POST request, send as buffer wil not work
+    req.write(s)
+    
+    
+    return d['html_response_code'], len(s), d 
+
+
+def update_node(req, path, params, data, id):
+    
+    #get the user and verify the signature
+    if params.get('user'):
+        user=users.getUser(params.get('user'))
+        userAccess = AccessData(user=user)
+        
+        if userAccess.user != None:
+            valid = userAccess.verify_request_signature(req.fullpath,params)
+            if valid == False:
+                userAccess = None
+        else:
+            userAccess = None
+    else:
+        user=users.getUser('Gast')
+        userAccess = AccessData(user=user)
+    
+    node = tree.getNode(id)
+    
+    #check user access
+    if userAccess != None and userAccess.hasAccess(node, "write"):
+        pass
+    else:
+        s = "No Access"
+        req.write(s)
+        d = {}
+        d['status'] = 'fail'
+        d['html_response_code'] = '403' # forbidden (authorization will not help)
+        d['errormessage'] = 'no access'
+        return d['html_response_code'], len(s), d 
+    
+    
+    node.name = params.get('name')
+    
+    metadata = json.loads(params.get('metadata'))
+    
+    #set provided metadata
+    for key,value in metadata.iteritems():
+        node.set(u(key),u(value))
+
+    # service flags
+    node.set("updateuser", user.getName())
+    node.set("updatetime", format_date())
+    node.setDirty()
+    
+    d = {}
+    d['status'] = 'OK'
+    d['html_response_code'] = '200' # ok
+    d['build_response_end'] = time.time()
+                   
+    s = "OK"
+    
+    #we need to write in case of POST request, send as buffer wil not work
+    req.write(s)
+    
+    req.reply_headers['updatetime'] = node.get('updatetime')
+    
+    return d['html_response_code'], len(s), d 
+
+
+
+# alternative base dir for static html files
+#
+# relative to mediatum folder:
+# WEBROOT="./web/services/static01/files/"
+#
+# absolute:
+# WEBROOT="/tmp/"
+
+#WEBROOT="./web/services/static01/files/" 
+
+# no WEBROOT configured, default will be used
+WEBROOT = None
+
+def serve_file(req, path, params, data, filepath):
+    atime = starttime = time.time()
+    
+    d = {}
+    d['timetable'] = []
+
+    if 'mimetype' in req.params:
+        mimetype = req.params['mimetype']
+    elif filepath.lower().endswith('.html') or filepath.lower().endswith('.htm'):
+        mimetype = 'text/html'
+    else:
+        mimetype = getMimeType(filepath)
+            
+    req.reply_headers['Content-Type'] = mimetype
+
+    if WEBROOT:
+        basedir = WEBROOT
+    else:
+        basedir = os.path.dirname(os.path.abspath( __file__ ))
+    abspath = os.path.join(basedir, 'static', filepath)
+    msg = "web service trying to serve: " + str(abspath)
+    logger.info(msg)
+    if os.path.isfile(abspath):
+        filesize = os.path.getsize(abspath)
+        req.sendFile(abspath, mimetype, force=1)
+        d['timetable'].append(["reading file '%s'" % filepath, time.time()-atime]); atime = time.time()
+        d['status'] = 'ok'
+        dataready = "%.3f" % (time.time() - starttime)
+        d['dataready'] = dataready
+        return 200, filesize, d # ok
+    else:
+        d['status'] = 'fail'
+        dataready = "%.3f" % (time.time() - starttime)
+        d['dataready'] = dataready        
+        return 404, 0, d # not found

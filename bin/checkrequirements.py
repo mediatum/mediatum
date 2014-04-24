999c1ed2b1023ff5fe4dd84d3cc3d164ebf9a38a
tobixx0 <Tobi.Stenzel@gmx.de>
2014-04-24 13:59:23 +0200

    fail if requests module is missing, closes #109

--- bin/checkrequirements.py
+++ bin/checkrequirements.py
@@ -123,7 +123,7 @@ python_module_descrs = [
   [["pymarc"], "pymarc: marc formatter for python", check_python_module, "RESTRICTION"],
   [["Levenshtein"], "python-Levenshtein: computing string distances and similarities", check_python_module, "RESTRICTION"],
   [["pyPdf"], "pyPdf: PDF toolkit", check_python_module, "RESTRICTION"],
-  [["requests"], "requests: HTTP request module, required for DOI import", check_python_module, "RESTRICTION"],
+  [["requests"], "requests: HTTP request module, required for DOI import", check_python_module, "FAIL"],
   [["pydot"], "pydot: python graphviz module, required for static workflow diagrams", check_python_module, "RESTRICTION"],
 ]
 

2951e1d2f1b800d34369e43fc6f36ec466dac382
Tobias Stenzel <tobias.stenzel@tum.de>
2014-10-08 08:43:42 +0200

    core.file improvements and fixes
    * type -> filetype
    * exception for old File args
    * open takes args now
    * fixed retrieveFile

--- core/file.py
+++ core/file.py
@@ -3,30 +3,34 @@
     :copyright: (c) 2014 by the mediaTUM authors
     :license: GPL3, see COPYING for details
 """
-
-from __future__ import division, absolute_import, print_function
 import os.path
 from warnings import warn
 
-from core.database.postgres import BaseFile
+from core.database.postgres import BaseFile, bref, rel
 from core import config
+from core.node import Node
 from utils.utils import get_filesize
+
+
+DATADIR = config.settings["paths.datadir"]
 
 
 class File(BaseFile):
 
+    node = rel(Node, backref=bref("files", lazy="dynamic"))
+
     def __init__(self, path, filetype, mimetype, node=None, **kwargs):
         # legacy stuff
-        if path.startswith(config.settings["paths.datadir"]):
+        if path.startswith(DATADIR):
             warn("file path starts with paths.datadir, should be relative", DeprecationWarning)
-            path = path[len(config.settings["paths.datadir"]):]
+            path = path[len(DATADIR):]
         if "type" in kwargs:
-            warn("type positional arg is deprecated, use filetype", DeprecationWarning)
+            raise Exception("type keyword arg is not allowed anymore, use filetype")
         if "filename" in kwargs:
-            warn("name positional arg is deprecated, use path", DeprecationWarning)
+            raise Exception("name positional arg is not allowed anymore, use path")
 
         self.path = path
-        self.type = filetype
+        self.filetype = filetype
         self.mimetype = mimetype
         if node is not None:
             self.node = node
@@ -41,14 +45,23 @@ class File(BaseFile):
         return os.path.basename(self.path)
 
     @property
+    def abspath(self):
+        return os.path.join(DATADIR, self.path)
+
+    @property
+    def type(self):
+        warn("use File.filetype instead", DeprecationWarning)
+        return self.filetype
+
+    @property
     def size(self):
         return get_filesize(self.path)
 
-    def open(self):
+    def open(self, *args, **kwargs):
         """Opens and returns the referenced file from the file system
         :raises: IOError
         """
-        return open(self.path)
+        return open(self.abspath, *args, **kwargs)
 
     def getType(self):
         warn("use File.type instead", DeprecationWarning)
@@ -63,10 +76,9 @@ class File(BaseFile):
         return self.base_name
 
     def retrieveFile(self):
-        warn("use File.open() instead, see doc", DeprecationWarning)
-        self.open()
+        warn("use File.open() or File.abspath instead, see doc", DeprecationWarning)
+        return self.abspath
 
     def getSize(self):
         warn("use File.size instead", DeprecationWarning)
         return self.size
-

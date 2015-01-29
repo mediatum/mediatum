7ac62e6828fce7bfe8674e24cd45046331d5aedb
Tobias Stenzel <tobias.stenzel@tum.de>
2015-01-29 17:10:00 +0000

    added option to ensure_unicode_return to silence warnings

--- utils/strings.py
+++ utils/strings.py
@@ -18,27 +18,30 @@ class EncodingException(Exception):
     pass
 
 
-def ensure_unicode_returned(f=None, name=None):
+def ensure_unicode_returned(f=None, name=None, silent=False):
     """
     
     """
     if f is None:
-        return partial(ensure_unicode_returned, name=name)
+        return partial(ensure_unicode_returned, name=name, silent=silent)
     
     if name is None:
         _name = f.__name__
     else:
         _name = name
-    
+        
     @wraps(f)
     def _wrapper(*args, **kwargs):
         res = f(*args, **kwargs)
         if isinstance(res, unicode):
             return res
         elif not isinstance(res, str):
-            logg.warn("return value of '%s': expected unicode, got value of type %s: '%s'", _name, type(res), res)
+            if not silent:
+                logg.warn("return value of '%s': expected unicode, got value of type %s: '%s'", _name, type(res), res)
             return unicode(res)
         else:
-            logg.warn("return value of '%s': expected unicode, trying to decode ustr as utf8: %s", _name, res[:200].encode("string-escape"))
+            if not silent:
+                logg.warn("return value of '%s': expected unicode, trying to decode ustr as utf8: %s", 
+                          _name, res[:200].encode("string-escape"))
             return unicode(res, encoding="utf8")
     return _wrapper

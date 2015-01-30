f8d3420d77eacdaa43d5f5e50bf69c3efa5c9e2b
Tobias Stenzel <tobias.stenzel@tum.de>
2015-01-30 10:32:22 +0000

    added ensure_unicode function
    same as decorator: converts utf8 strings and other non-unicode values to unicode,
    warns if silent=False

--- utils/strings.py
+++ utils/strings.py
@@ -43,5 +43,18 @@ def ensure_unicode_returned(f=None, name=None, silent=False):
             if not silent:
                 logg.warn("return value of '%s': expected unicode, trying to decode ustr as utf8: %s", 
                           _name, res[:200].encode("string-escape"))
-            return unicode(res, encoding="utf8")
+            return res.decode("utf8")
     return _wrapper
+
+
+def ensure_unicode(s, silent=False):
+    if isinstance(s, unicode):
+        return s
+    elif not isinstance(s, str):
+        if not silent:
+            logg.warn("expected unicode, got value of type %s: '%s'", type(s), s)
+        return unicode(s)
+    else:
+        if not silent:
+            logg.warn("trying to decode ustr as utf8: %s", s[:200].encode("string-escape"))
+        return s.decode("utf8")

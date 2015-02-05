bb97258f20ab2d4ebe4aa8b5a5b843bec66d8cec
Tobias Stenzel <tobias.stenzel@tum.de>
2015-02-05 16:14:15 +0000

    Added trace logger
    squash commit of:
    
    commit 3c752c6a1678c88bd34a87d6cde8c69d8834201f
    Author: Tobias Stenzel <tobias.stenzel@tum.de>
    Date:   Thu Jan 22 14:33:09 2015 +0000
    
        makeRecord adds extra information for Exceptions
    
        * all members of the exception object and the type are passed as extra args
        * highlight and escape the byte sequence that caused an UnicodeError
    
    commit eaf6b5a4827a34e027a7ca1a6e38599a64bfe88c
    Author: Tobias Stenzel <tobias.stenzel@tum.de>
    Date:   Mon Dec 29 13:03:50 2014 +0000
    
        added TraceLogger and support for it in ConsoleLogger
    
        * TraceLogger adds an extra field `trace` with a stack trace if
            * if requested in the logging call
            * if logging level is at least `trace_level`
    
        * ConsoleLogger uses trace as exc_text

--- utils/log.py
+++ utils/log.py
@@ -22,14 +22,14 @@ from core import config
 import logging
 import sys
 import logstash
+import inspect
+import traceback
 
 
 ROOT_STREAM_LOGFORMAT = '%(asctime)s [%(process)d/%(threadName)s] %(name)s %(levelname)s | %(message)s'
 # this also logs filename and line number, which is great for debugging
 # ROOT_STREAM_LOGFORMAT = '%(asctime)s %(name)s/%(module)s [%(threadName)s] %(levelname)s | %(message)s - %(pathname)s:%(lineno)d'
 ROOT_FILE_LOGFORMAT = ROOT_STREAM_LOGFORMAT
-
-logg = logging.getLogger(__name__)
 
 # path's will be required in web/admin/modules/logfile.py
 dlogfiles = {}
@@ -46,6 +46,8 @@ class ConsoleHandler(logging.StreamHandler):
     ConsoleHandler behaves.
     
     from: http://code.activestate.com/recipes/576819-logging-to-console-without-surprises/ 
+    
+    added: copy trace attribute to exc_text for TraceLogger support
     """
 
     def __init__(self):
@@ -53,6 +55,9 @@ class ConsoleHandler(logging.StreamHandler):
         self.stream = None  # reset it; we are not going to use it anyway
 
     def emit(self, record):
+        if hasattr(record, "trace"):
+            record.exc_text = record.trace
+            
         if record.levelno >= logging.ERROR:
             self.__emit(record, sys.stderr)
         else:
@@ -61,6 +66,88 @@ class ConsoleHandler(logging.StreamHandler):
     def __emit(self, record, strm):
         self.stream = strm
         logging.StreamHandler.emit(self, record)
+
+
+class TraceLogger(logging.Logger):
+        
+    """Adds an optional traceback for some messages.
+        
+    A traceback is added if the logging level is at least `trace_level` 
+    or requested in the logging call (if `trace` is a true value). 
+    """
+    
+    _trace_level = logging.WARN 
+    
+    @property
+    def trace_level(self, level):
+        """All log messages above the given `level` will have an additional field with a stack trace up to the logging call
+        """  
+        self._trace_level = level
+    
+    def _log(self, level, msg, args, exc_info=None, extra=None, trace=None):
+        """Adds an optional traceback for some messages and calls Logger._log.
+        A traceback is added if the logging level is at least `trace_level` or requested in the logging call. 
+        
+        :param trace: Always add a trace if trace is true
+        """
+        if trace or (level >= self._trace_level and not exc_info):
+            if extra is None:
+                extra = {}
+            f = inspect.currentframe()
+            tblines = traceback.format_stack(f)
+            
+            # nice hack to shorten long and ugly athana handler stack traces ;)
+            for start_lineno, line in enumerate(tblines):
+                if ", in call_and_close" in line:
+                    break
+            else:
+                start_lineno = 0
+                
+            # cut off calls in the logging module
+            if "_showwarning" in tblines[-3]:
+                end_lineno = -3
+            else:
+                end_lineno = -2
+                
+            tbtext = "".join(tblines[start_lineno:end_lineno])
+            extra["trace"] = tbtext
+            
+        logging.Logger._log(self, level, msg, args, exc_info=exc_info, extra=extra)
+        
+    def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None):
+        if exc_info:
+            if extra is None:
+                extra = {}
+            
+            # get additional info about the exception
+            ex_type, ex, _ = exc_info     
+            extra["exc_type"] = ex_type.__name__
+            # add members of the exception as extra info except callables and non-public stuff
+            for name, val in inspect.getmembers(ex, lambda a: not callable(a)):
+                # overriding log message is not allowed, rename it
+                if name == "message":
+                    extra["exc_message"] = val
+                # args just contains all exception args again, can be ignored
+                elif not name.startswith("_") and name not in ("args",):
+                    extra[name] = val
+        
+            if issubclass(ex_type, UnicodeError):
+                start = extra["start"]
+                end = extra["end"]
+                obj = extra["object"]
+                
+                # escape string which caused this exception
+                error_part = obj[start:end+1].encode("string_escape")
+                extra["object"] = obj[:start-1] + "[ERROR]" + error_part + "[/ERROR]" + obj[end+1:]
+        
+        return logging.Logger.makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=func, extra=extra)
+
+### init
+logging.setLoggerClass(TraceLogger)
+logging.captureWarnings(True)
+
+# get the logger after setting the logger class!
+logg = logging.getLogger(__name__)
 
 
 def initialize():
@@ -80,8 +167,6 @@ def initialize():
     
     logstash_handler = logstash.TCPLogstashHandler("localhost", 5959, version=1, message_type="mediatum")
     root_logger.addHandler(logstash_handler)
-
-
     filepath = config.get('logging.file', None)
         
     if filepath:

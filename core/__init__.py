f1a0deedd44f3047ec091559b5f1f27aab27ab43
Tobias Stenzel <tobias.stenzel@tum.de>
2015-02-18 18:37:38 +0000

    fix for git merge fail in core/__init__

--- core/__init__.py
+++ core/__init__.py
@@ -1,16 +1,3 @@
-import os
-import sys
-
-editmodulepaths = [('', 'web/edit/modules')]
-
-import utils.log
-
-from core import config
-
-config.initialize()
-
-from core import acl
-from core import users
 # set by core.init.init_app()
 db = None
 app = None

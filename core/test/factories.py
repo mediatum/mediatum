f023b04963f079d46941fe5dbbc1eebf30709ed7
Tobias Stenzel <tobias.stenzel@tum.de>
2015-02-27 13:05:35 +0000

    fixed name generation in NodeFactory
    o.type is not present at creation time, so we have to be a bit more
    creative... Is there a better way?

--- core/test/factories.py
+++ core/test/factories.py
@@ -3,7 +3,9 @@
     :copyright: (c) 2014 by the mediaTUM authors
     :license: GPL3, see COPYING for details
 """
+import string
 import factory.alchemy
+from factory import fuzzy
 
 from contenttypes import Directory
 from contenttypes import Document
@@ -22,13 +24,22 @@ class NodeFactory(SQLAFactory):
         model = Node
     id = factory.Sequence(lambda n: n)
     orderpos = 1
-    name = factory.LazyAttributeSequence(lambda o, n: u"{}#{}".format(o.type, n))
+    
+    # XXX: is there a better way for getting the model name? This is so crazy...
+    @classmethod
+    def model_name(cls):
+        return cls._meta.model.__name__
+    
+    name = factory.LazyAttributeSequence(lambda o, n: u"{}#{}".format(o._LazyStub__model_class.model_name(), n))
 
 
 class UserFactory(NodeFactory):
     class Meta:
         model = User
-
+    
+    attrs = factory.Dict({
+        "service.userkey": fuzzy.FuzzyText(length=6, chars=string.lowercase)
+    })
 
 class DocumentFactory(NodeFactory):
     class Meta:

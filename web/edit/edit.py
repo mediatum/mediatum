f17eafb846d1a00f3196c422799f0f7868157b89
Andrew Darrohn <andrew.darrohn@tum.de>
2015-03-25 13:21:30 +0100

    fixes getChildren() call

--- web/edit/edit.py
+++ web/edit/edit.py
@@ -354,7 +354,7 @@ def getIDs(req):
 def nodeIsChildOfNode(node1, node2):
     if node1.id == node2.id:
         return 1
-    for c in node2.getChildren():
+    for c in node2.children:
         if nodeIsChildOfNode(node1, c):
             return 1
     return 0

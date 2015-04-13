2b37bc881fe9d95c3d2c785cb419968d1ea8433d
Tobias Stenzel <tobias.stenzel@tum.de>
2015-04-13 11:28:56 +0000

    plpgsql utils: function deletion function

---
+++ core/database/postgres/sql/utils.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE FUNCTION public.f_delfunc(_schema text, _del text = '') 
+  RETURNS text
+  LANGUAGE plpgsql 
+AS
+$BODY$
+DECLARE
+    _sql   text;
+    _ct    text;
+
+BEGIN
+   SELECT INTO _sql, _ct
+          string_agg('DROP '
+                   || CASE p.proisagg WHEN true THEN 'AGGREGATE '
+                                                ELSE 'FUNCTION ' END
+                   || quote_ident(n.nspname) || '.' || quote_ident(p.proname)
+                   || '('
+                   || pg_catalog.pg_get_function_identity_arguments(p.oid)
+                   || ');'
+                  ,E'\n'
+          )
+          ,count(*)::text
+   FROM   pg_catalog.pg_proc p
+   LEFT   JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+   WHERE  n.nspname = _schema;
+   -- AND p.proname ~~* 'f_%';                     -- Only selected funcs?
+   -- AND pg_catalog.pg_function_is_visible(p.oid) -- Only visible funcs?
+
+IF lower(_del) = 'del' THEN                        -- Actually delete!
+   EXECUTE _sql;
+   RETURN _ct || E' functions deleted:\n' || _sql;
+ELSE                                               -- Else only show SQL.
+   RETURN _ct || E' functions to delete:\n' || _sql;
+END IF;
+
+END;
+$BODY$;
+

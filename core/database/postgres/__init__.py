f7377c35094a692abb1f006a50e5b85dc9404b49
Tobias Stenzel <tobias.stenzel@tum.de>
2015-04-29 14:41:13 +0000

    added TimeStamp, integer_pk / fk helpers
    * TimeStamp mixin for declarative
    * integer_pk / fk generate primary / foreign columns for declarative

--- core/database/postgres/__init__.py
+++ core/database/postgres/__init__.py
@@ -9,9 +9,9 @@ import pyaml
 import sqlalchemy as sqla
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine import Engine
-from sqlalchemy import event
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, ForeignKey, event, Integer, DateTime, func
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declared_attr
 
 
 logg = logging.getLogger(__name__)
@@ -21,6 +21,27 @@ C = Column
 FK = ForeignKey
 rel = relationship
 bref = backref
+
+class TimeStamp(object):
+
+    """a simple timestamp mixin"""
+
+    @declared_attr
+    def created_at(cls):
+        return C(DateTime, default=func.now())
+
+
+def integer_pk(**kwargs):
+    return C(Integer, primary_key=True, **kwargs)
+
+
+def integer_fk(*args, **kwargs):
+    if len(args) == 2:
+        return C(args[0], ForeignKey(args[1]), **kwargs)
+    elif len(args) == 1:
+        return C(ForeignKey(args[0]), **kwargs)
+    else:
+        raise ValueError("at least one argument must be specified (type)!")
 
 
 db_metadata = sqla.MetaData(schema="mediatum")

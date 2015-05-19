2e7216e2fdd7efec50ef32d77777b1532ec194cc
Tobias Stenzel <tobias.stenzel@tum.de>
2015-05-19 18:19:26 +0000

    Drop/CreateView refactoring, added DropTableCascade
    * use _CreateDropBase for custom DDL elements
    * DropTableCascade drops with cascade...

--- core/database/postgres/alchemyext.py
+++ core/database/postgres/alchemyext.py
@@ -1,44 +1,49 @@
 # -*- coding: utf-8 -*-
 """
     SQLAlchemy extensions
-    
+
     * new types
     * compiler extension
 
     :copyright: (c) 2014 by the mediaTUM authors
     :license: GPL3, see COPYING for details
 """
+import logging
 from warnings import warn
 
 from sqlalchemy.ext import compiler
 from sqlalchemy.sql import table
-from sqlalchemy.sql.ddl import DDLElement
+from sqlalchemy.sql.ddl import CreateTable, _CreateDropBase, DropTable, sort_tables
 from sqlalchemy.sql.elements import quoted_name
 from sqlalchemy.sql.type_api import UserDefinedType
 from sqlalchemy.orm.dynamic import AppenderQuery
+from core.database.postgres import DeclarativeBase, db_metadata
+from utils.compat import string_types
+from sqlalchemy import Table
 
 
-class CreateView(DDLElement):
+logg = logging.getLogger(__name__)
 
-    def __init__(self, name, selectable):
-        self.name = name
+
+class CreateView(_CreateDropBase):
+
+    def __init__(self, element, selectable, on=None, bind=None):
         self.selectable = selectable
+        super(CreateView, self).__init__(element, on, bind)
 
 
-class DropView(DDLElement):
-
-    def __init__(self, name):
-        self.name = name
+class DropView(_CreateDropBase):
+    pass
 
 
 @compiler.compiles(CreateView)
-def visiit_create_view(element, compiler, **kw):
-    return "CREATE VIEW %s AS %s" % (element.name, compiler.sql_compiler.process(element.selectable))
+def visit_create_view(ddl_element, compiler, **kw):
+    return "CREATE VIEW %s AS %s" % (ddl_element.element.fullname, compiler.sql_compiler.process(ddl_element.selectable))
 
 
 @compiler.compiles(DropView)
-def visit_drop_view(element, compiler, **kw):
-    return "DROP VIEW %s" % (element.name)
+def visit_drop_view(ddl_element, compiler, **kw):
+    return "DROP VIEW %s" % (ddl_element.element.fullname)
 
 
 def view(name, metadata, selectable):
@@ -53,9 +58,18 @@ def view(name, metadata, selectable):
     for c in selectable.c:
         c._make_proxy(t)
 
-    CreateView(full_name, selectable).execute_at('after-create', metadata)
-    DropView(full_name).execute_at('before-drop', metadata)
+    CreateView(t, selectable).execute_at('after-create', metadata)
+    DropView(t).execute_at('before-drop', metadata)
     return t
+
+
+class DropTableCascade(_CreateDropBase):
+    pass
+
+
+@compiler.compiles(DropTableCascade)
+def visit_drop_table_cascade(ddl_element, compiler, **kw):
+    return "DROP TABLE IF EXISTS %s CASCADE" % (ddl_element.element.fullname)
 
 
 class LenMixin(object):

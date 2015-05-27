510e3e69afb2b80f2d84de2e87025da7405f9ced
Tobias Stenzel <tobias.stenzel@tum.de>
2015-05-27 15:30:28 +0000

    column changes in permission model classes
    * we don't need AccessRule.name
    * blocking and inherited are properties of the node to rule relation

--- core/database/postgres/permission.py
+++ core/database/postgres/permission.py
@@ -15,15 +15,12 @@ class AccessRule(DeclarativeBase):
     __tablename__ = "access_rule"
 
     id = integer_pk()
-    name = C(Unicode, index=True)
-    blocking = C(Boolean, default=False, index=True)
     invert_subnet = C(Boolean, default=False, index=True)
     invert_date = C(Boolean, default=False, index=True)
     invert_group = C(Boolean, default=False, index=True)
     group_ids = C(ARRAY(Integer), index=True)
     subnets = C(ARRAY(CIDR), index=True)
     dateranges = C(ARRAY(Daterange), index=True)
-    inherited = C(Boolean, default=False, index=True)
 
 
 class NodeToAccessRule(DeclarativeBase):
@@ -31,9 +28,10 @@ class NodeToAccessRule(DeclarativeBase):
 
     nid = integer_fk(Node.id, primary_key=True)
     rule_id = integer_fk(AccessRule.id, primary_key=True)
-    ruletype = C(Text, index=True)
+    ruletype = C(Text, index=True, primary_key=True)
     invert = C(Boolean, default=False, index=True)
     inherited = C(Boolean, default=False, index=True)
+    blocking = C(Boolean, default=False, index=True)
 
     rule = rel(AccessRule, backref="node_assocs")
 

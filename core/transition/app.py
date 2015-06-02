69f6554d35dd39849bdbe4a571e3d2a0258d9b03
Tobias Stenzel <tobias.stenzel@tum.de>
2015-06-02 16:01:22 +0000

    set fake ip address (localhost) in AthanaTestRequest

--- core/transition/app.py
+++ core/transition/app.py
@@ -31,6 +31,7 @@ class AthanaTestRequest(athana_http.http_request):
         athana_http.http_request.__init__(self, None, None, None, uri, 0, headers)
         self.request_headers = headers
         self.params = params or {}
+        self.ip = "127.0.0.1"
 
     @property
     def text(self):

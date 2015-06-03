2a7d452911e817b69e2231eb92ab746b47efece6
Tobias Stenzel <tobias.stenzel@tum.de>
2015-06-03 14:08:29 +0000

    transition: call Node.__init__ instead of super in node class decorators
    * super doesn't work when subclassing container types, use Node directly

--- core/transition/postgres.py
+++ core/transition/postgres.py
@@ -7,6 +7,7 @@ from warnings import warn
 
 
 def check_type_arg(cls):
+    from core.database.postgres.node import Node
     clsname = cls.__name__
 
     def init(self, name="", type=None, id=None, attrs=None, orderpos=None):
@@ -17,13 +18,14 @@ def check_type_arg(cls):
             if not type == clsname.lower():
                 raise ValueError("type must be {} for a {} instance ".format(clsname.lower(), clsname))
 
-        super(cls, self).__init__(name, type, id, None, attrs, orderpos)
+        Node.__init__(self, name, type, id, None, attrs, orderpos)
 
     cls.__init__ = init
     return cls
 
 
 def check_type_arg_with_schema(cls):
+    from core.database.postgres.node import Node
     clsname = cls.__name__
 
     def init(self, name="", type=None, id=None, schema=None, attrs=None, orderpos=None):
@@ -34,7 +36,7 @@ def check_type_arg_with_schema(cls):
             if not type.startswith(clsname.lower()):
                 raise ValueError("type must be {} for a {} instance ".format(clsname.lower(), clsname))
 
-        super(cls, self).__init__(name, type, id, schema, attrs, orderpos)
+        Node.__init__(self, name, type, id, schema, attrs, orderpos)
 
     cls.__init__ = init
     return cls

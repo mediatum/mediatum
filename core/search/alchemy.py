d047da93f2480609358100188485b18c503f917e
Tobias Stenzel <tobias.stenzel@tum.de>
2015-06-30 11:32:26 +0000

    alchemy.walk produces correct search queries for fulltext and attr

--- core/search/alchemy.py
+++ core/search/alchemy.py
@@ -8,6 +8,8 @@
 from sqlalchemy import func as sqlfunc, and_, or_
 from core.search.representation import AttributeMatch, FullMatch, SchemaMatch, FulltextMatch, AttributeCompare, TypeMatch, And, Or, Not
 from core import Node
+from core.database.postgres import func
+
 
 comparisons = {
     ">": (lambda l, r: l > r),
@@ -20,15 +22,15 @@ comparisons = {
 language = "german"
 
 
-def make_fulltext_expression_tsvec(language, target, searchterm, op="|"):
+def make_fts_expr(language, target, searchterm, op="|"):
     """Searches fulltext column, building ts_vector on the fly.
     `target` must have a gin index built with an ts_vector or this will be extremly slow.
     :param language: postgresql language string
-    :param target: SQLAlchemy expression pointing to a text column
+    :param target: SQLAlchemy expression with type text
     :param searchterm: string of space-separated words to search
     :param op: operator used to join searchterms separated by space, | or &
     """
-    tsvec = sqlfunc.to_tsvector(language, target)
+    tsvec = func.to_tsvector_safe(language, target)
     ts_query = sqlfunc.to_tsquery(language, op.join(searchterm.split()))
     return tsvec.op("@@")(ts_query)
 
@@ -45,16 +47,16 @@ def walk(n):
         return or_(left, right)
 
     elif isinstance(n, AttributeMatch):
-        return make_fulltext_expression_tsvec(language,
+        return make_fts_expr(language,
                                               Node.attrs[n.attribute].astext,
                                               n.searchterm)
 
     elif isinstance(n, FulltextMatch):
-        return make_fulltext_expression_tsvec(language, Node.fulltext, n.searchterm)
+        return make_fts_expr(language, Node.fulltext, n.searchterm)
 
     elif isinstance(n, FullMatch):
-        fulltext_cl = make_fulltext_expression_tsvec(language, Node.fulltext, n.searchterm)
-        attrs_cl = make_fulltext_expression_tsvec(language, Node.attrs, n.searchterm)
+        fulltext_cl = make_fts_expr(language, Node.fulltext, n.searchterm)
+        attrs_cl = make_fts_expr(language, func.json_object_values_text_concat(Node.attrs, "|"), n.searchterm)
         return or_(fulltext_cl, attrs_cl)
 
     elif isinstance(n, AttributeCompare):

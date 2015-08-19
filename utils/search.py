ddcbdea70266a98d8ae850092ac8668672d875e4
Tobias Stenzel <tobias.stenzel@tum.de>
2015-08-19 17:18:21 +0200

    node fulltext import in python
    * just load the fulltext files referenced by nodes and write them via the ORM
    * bit slower than other methods, but fast enough ;)
    * alternative: use SQL functions in nodesearch.sql

---
+++ utils/search.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: (c) 2015 by the mediaTUM authors
+    :license: GPL3, see COPYING for details
+"""
+
+import logging
+from core import db, File
+from contenttypes import Data
+
+q = db.query
+logg = logging.getLogger(__name__)
+
+
+def import_node_fulltext(node, overwrite=False):
+    s = db.session
+
+    if not overwrite and node.fulltext:
+        logg.info("node %s already has a fulltext, not overwriting it", node.id)
+        return False
+
+    fulltext_files = node.files.filter_by(filetype=u"fulltext")
+    fulltexts = []
+
+    for fi in fulltext_files:
+        if fi.exists:
+            with fi.open() as f:
+                fulltexts.append(f.read())
+
+            logg.debug("imported fulltext for node %s from %s", node.id, fi.path)
+        else:
+            logg.warn("missing fulltext for node %s from %s", node.id, fi.path)
+
+    if fulltexts:
+        node.fulltext = u"\n---\n".join(fulltexts)
+        s.commit()
+        return True
+
+    return False
+
+
+def import_fulltexts(overwrite=False):
+    if overwrite:
+        nodes = q(Data).join(File).filter_by(filetype=u"fulltext")
+    else:
+        nodes = q(Data).filter_by(fulltext=None).join(File).filter_by(filetype=u"fulltext")
+
+    import_count = 0
+    for node in nodes:
+        imported = import_node_fulltext(node)
+        if imported:
+            import_count += 1
+
+    return import_count

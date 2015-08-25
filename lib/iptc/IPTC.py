c354e5894f560bfacf7e90c3fd42a103cf4bf1f9
Tobias Stenzel <tobias.stenzel@tum.de>
2015-08-25 12:55:38 +0200

    Revert "sync changed iptc tags with image files, closes #487"
    This reverts commit c828540ac408a8280646f4a82642fcba53a693f4.
    
    * change to exiftool, this has too many problems...

--- lib/iptc/IPTC.py
+++ lib/iptc/IPTC.py
@@ -1,124 +1,179 @@
-import logging
-import os
-from TBP import Jpeg, Tiff
+#!/usr/bin/python
+# Python IPTC Parser
+# By Jean-Francois Roche <jfroche@jfroche.be>
+# ZPhotoSlides, Zope product
+# Copyright (C) 2002-2004  Jean-Francois Roche <jfroche@jfroche.be>
+#                          Florent Carlier <florent.carlier@insa-rennes.fr>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Id: IPTC.py,v 1.2 2009/10/08 09:09:23 seiferta Exp $'
+# Based on a PHP script for IPTC parser this avoid to install PIL
+import sys
+from StringIO import StringIO
 
 
-logger = logging.getLogger('editor')
+TagsNames = {
+    0x14: 'SuplementalCategories',
+    0x19: 'Keywords',
+    0x78: 'Caption',
+    0x7A: 'CaptionWriter',
+    0x69: 'Headline',
+    0x28: 'SpecialInstructions',
+    0x0F: 'Category',
+    0x50: 'Byline',
+    0x55: 'BylineTitle',
+    0x6E: 'Credit',
+    0x73: 'Source',
+    0x74: 'CopyrightNotice',
+    0x05: 'ObjectName',
+    0x5A: 'City',
+    0x5F: 'ProvinceState',
+    0x65: 'CountryName',
+    0x67: 'OriginalTransmissionReference',
+    0x37: 'DateCreated',
+    0x0A: 'CopyrightFlag'
+}
+
+try:
+    f = False
+except:
+    False = -1
+    True = 1
 
 
-def get_wanted_iptc_tags():
-    """
-       Returns a dictionary of the IPTC Application Record tags
-
-       :return: dictionary
-    """
-    return {
-        'ApplicationRecordVersion': 'ApplicationRecordVersion: mandatory / the current IPTC Information Interchange Model = 4',
-        'ObjectTypeReference': 'ObjectTypeReference: 1:News, 2:Data, 3:Advisory x:yy',
-        'ObjectAttributeReference': 'ObjectAttributeReference: 1:Current, 2:Analysis, 3: Archive ..x:yy',
-        'ObjectName': 'ObjectName: short reference on the object',
-        'EditStatus': 'EditStatus: status of the object',
-        'EditorialUpdate': 'EditorialUpdate: (To a previous object) x:yyy (Num)',
-        'Urgency': 'Urgency: highest is 1, 5 is normal and 8 is lowest',
-        'SubjectReference': 'SubjectReference: IPTC:SubRefNum:SubName:SubMatter:SubDetailName',
-        'Category': 'Category: not supported anymore,examples: ACE, EDU, SPO for Arts Culture Entertainment,Education,Sport',
-        'SupplementalCategories': 'SupplementalCategories: free chosable categories',
-        'FixtureIdentifier': 'Fixture Identifier: identifies frequently occuring object data eg "Euroweather"',
-        'Keywords': 'Keywords: for in example search purposes',
-        'ContentLocationCode': 'ContentLocationCode: 3 char code indicating which country the event took place',
-        'ContentLocationName': 'ContentLocationName: Name of the country the event took place',
-        'ReleaseDate': 'ReleaseDate: the earliest date the provider intends the object is to be used in format: YYYYMMDD',
-        'ReleaseTime': 'ReleaseTime: the earliest time the provider intends the object is to be used in format: HHMMSS +- HHMM [120015+0100]',
-        'ExpirationDate': 'ExpirationDate: the latest date the provider intends the object is to be used in format: YYYYMMDD',
-        'ExpirationTime': 'ExpirationTime: the latest time the provider intends the object is to be used in format: HHMMSS +- HHMM [120015+0100]',
-        'SpecialInstructions': 'SpecialInstructions: further instructions on the object',
-        'ActionAdvised': 'ActionAdvised: 2 Digits - provider defined eg 01',
-        'ReferenceService': 'ReferenceService: only used to refer to a previous 1:30',
-        'ReferenceDate': 'ReferenceDate: only allowed if ReferenceService is used  in format: YYYYMMDD',
-        'ReferenceNumber': 'ReferenceNumber: only allowed if ReferenceService is used',
-        'DateCreated': 'DateCreated: creation date in format: YYYYMMDD',
-        'TimeCreated': 'TimeCreated: creation time in format: HHMMSS +- HHMM [120015+0100]',
-        'DigitalCreationDate': 'DigitalCreationDate: date for digitalisation in format: YYYYMMDD',
-        'DigitalCreationTime': 'DigitalCreationTime: time for digitalisation in format: HHMMSS +- HHMM [120015+0100]',
-        'OriginatingProgram': 'OriginatingProgram: the software used for creation [mediatum]',
-        'ProgramVersion': 'ProgramVersion: only use if OriginatingProgram used eg "1.2"',
-        'ObjectCycle': 'ObjectCycle: virtually only used in US for Morning Evening or both',
-        'By-line': 'By-line: author or photograph',
-        'By-lineTitle': 'By-lineTitle: job title',
-        'City': 'City: source location of the object',
-        'Sub-location': 'Sub-location: district of the location',
-        'Province-State': 'Province-State: province',
-        'Country-PrimaryLocationCode': 'Country-PrimaryLocationCode: code like in iso 3166 [DE, DEU]',
-        'Country-PrimaryLocationName': 'Country-PrimaryLocationName: full name of the country',
-        'OriginalTransmissionReference': 'OriginalTransmissionReference: job identifier',
-        'Headline': 'Headline: headline',
-        'Credit': 'Credit: provider of the object',
-        'Source': 'Source: source can differ from creater',
-        'CopyrightNotice': 'CopyrightNotice: copyright notice',
-        'Contact': 'Contact: contact for further information on the object',
-        'Caption-Abstract': 'Caption-Abstract: short description',
-        'LocalCaption': 'LocalCaption: local caption',
-        'Writer-Editor': 'Writer-Editor: name of the editor',
-        'RasterizedCaption': 'RasterizedCaption: rasterized caption',
-        'ImageType': 'ImageType: code representing B/w, Y comp of seps',
-        'ImageOrientation': 'ImageOrientation: P = Portrait, L = landscape, S = Square',
-        'LanguageIdentifier': 'LanguageIdentifier: short code identifying the language',
-        'AudioType': 'AudioType: type of the audio',
-        'AudioSamplingRate': 'AudioSamplingRate: audio sampling rate',
-        'AudioSamplingResolution': 'AudioSamplingResolution: audio sampling resolution',
-        'AudioDuration': 'AudioDuration: audio duration',
-        'AudioOutcue': 'AudioOutcue: audio octune',
-        'JobID': 'JobID: job id',
-        'MasterDocumentID': 'MasterDocumentID: master document id',
-        'ShortDocumentID': 'ShortDocumentID: short document id',
-        'UniqueDocumentID': 'UniqueDocumentID: unique document id',
-        'OwnerID': 'OwnerID: owner id',
-        'ObjectPreviewFileFormat': 'ObjectPreviewFileFormat: object prieview file format',
-        'ObjectPreviewFileVersion': 'ObjectPreviewFileVersion: object preview file version',
-        'ObjectPreviewData': 'ObjectPreviewData: object preview data',
-        'ClassifyState': 'ClassifyState: classify state',
-        'SimilarityIndex': 'SimilarityIndex: similarity index',
-        'DocumentNotes': 'DocumentNotes: document notes',
-        'DocumentHistory': 'DocumentHistory: document history',
-        'ExifCameraInfo': 'ExifCameraInfo: technical camera info'}
+def i16(c, o=0):
+    return ord(c[o + 1]) + (ord(c[o]) << 8)
 
 
-def get_iptc_object(file_path):
-    """
-        returns a itpc object
-        for a given path
-        (only jpg and tiff are supported)
-
-        :rtype : object
-        :param file_path: path to tht file
-        :return: iptc object: iptc object to the given path
-    """
-    if os.path.splitext(file_path)[-1].lower() in ['.jpg', '.jpeg']:
-        return Jpeg(file_path)
-
-    elif os.path.splitext(file_path)[-1].lower() in ['.tif', '.tiff']:
-        return Tiff(file_path)
-    else:
-        logger.info('Unsupported file for IPTC reading.')
-        return None
+def i32(c, o=0):
+    return ord(c[o + 3]) + (ord(c[o + 2]) << 8) + (ord(c[o + 1]) << 16) + (ord(c[o]) << 24)
 
 
-def get_iptc_values(file_path):
-    """
-        get's the IPTC tags/values from a given
-        image filepath
+def getiptc_path(path):
+    file = open(path, 'rb')
+    return getiptc(file)
 
-        :rtype : object
-        :param file_path: path to the desired file
-        :return: dictionary with tags
-    """
-    iptc_object = get_iptc_object(file_path)
-    ret = {}
-    if iptc_object != {}:
-        for tag in get_wanted_iptc_tags():
-            try:
-                if iptc_object.getIPTCTag(tag):
-                    ret[tag] = iptc_object.getIPTCTag(tag)
-            except AttributeError as e:
-                logging.log('Attribute Error: ', e)
-    return ret
+
+def getiptc_data(data):
+    return getiptc(StringIO(data))
+
+
+def getiptc(file):
+    data = file.read(1)
+    if(ord(data) != 0xFF):
+        return {}
+    data = file.read(1)
+    if(ord(data) != 0xD8):
+        return {}
+    count = 0
+    done = False
+    ok = True
+    _markers = {}
+    while(not done):
+        capture = False
+        discarded = 0
+        c = ord(file.read(1))
+        while(c != 0xFF):
+            discarded = discarded + 1
+            c = ord(file.read(1))
+        marker = ord(file.read(1))
+        while(marker == 0xFF):
+            marker = ord(file.read(1))
+        if (discarded != 0):
+            break
+        length = (ord(file.read(1)) * 256 + ord(file.read(1)))
+        if (length < 2):
+            break
+
+        if(marker == 0xC0 or marker == 0xC1 or marker == 0xC2 or marker == 0xC9 or marker == 0xE0 or marker == 0xE1):
+            pass  # dont care about them
+        elif(marker == 0xED):    # SOF1
+            capture = True
+            done = True
+        elif(marker == 0xDA):  # SOS: Start of scan... the image itself and the last block on the file
+            capture = False
+            length = -1  # This field has no length... it includes all data until EOF
+            done = True
+        else:
+            capture = True
+        _markers[count] = {}
+        _markers[count]['marker'] = marker
+        _markers[count]['length'] = length
+        if (capture):
+            _markers[count]['data'] = file.read(length)
+        elif (not done):  # result = @fseek($this->_fp, $length, SEEK_CUR);
+            file.seek(length - 2, 1)
+        count = count + 1
+
+    data = 0
+    for key in _markers.keys():
+        dict = _markers[key]
+        if(dict['marker'] == 0xED):
+            tmpdata = StringIO(dict['data'])
+            signature = tmpdata.read(14)
+            if(signature == "Photoshop 3.0\0"):
+                data = dict['data']
+                break
+    if (data == 0):
+        return {}
+    pos = 14
+    datasize = len(data)
+    _markers['iptc'] = {}
+    while (pos < datasize):
+        signature = data[pos:pos + 4]
+        if (signature != '8BIM'):
+            break
+        pos = pos + 4
+        types = i16(data, pos)
+        pos = pos + 2
+        strlen = ord(data[pos])
+        pos = pos + 1
+        header = data[pos:pos + strlen]
+        pos = pos + strlen + 1 - (strlen % 2)  # The string is padded to even length, counting the length byte
+        length = i32(data, pos)
+        pos = pos + 4
+        basePos = pos
+        if(types == 0x404):
+            # we have iptc here!
+            while (pos < (datasize - 5)):
+                signature = i16(data, pos)
+                if (signature != 0x1C02):
+                    break
+                pos = pos + 2
+                types = ord(data[pos])
+                pos = pos + 1
+                length = i16(data, pos)
+                pos = pos + 2
+                basePos = pos
+                label = ''
+                if (TagsNames.has_key(types)):
+                    label = TagsNames[types]
+                else:
+                    label = 'IPTC_%s' % types
+                if(_markers['iptc'].has_key(label)):
+                    if(type(_markers['iptc'][label]) != type([])):
+                        tmp = _markers['iptc'][label]
+                        _markers['iptc'][label] = []
+                        _markers['iptc'][label].append(tmp)
+                    _markers['iptc'][label].append(data[pos:pos + length])
+                else:
+                    _markers['iptc'][label] = data[pos:pos + length]
+                pos = basePos + length
+    return _markers['iptc']
+
+
+def getIPTCValues(filename):
+    return getiptc_path(filename)
--- lib/iptc/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-    :copyright: (c) 2015 by the mediaTUM authors
-    :license: GPL3, see COPYING for details
-"""

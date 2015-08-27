71ced659e2643bfb70bb56e9e8a76eea58c916dd
Tobias Stenzel <tobias.stenzel@tum.de>
2015-08-27 18:09:47 +0200

    ldapauth basics work
    * extracted all config options to module globals
        * can be replaced by database config values later
    *
    * get display name and telephone number from LDAP data
    * dir id (ldap.user_login) is set as login_name
    * renamed some config attributes
    
    missing:
    
    * remove auto groups only
    * multiple group attrs

--- core/ldapauth.py
+++ core/ldapauth.py
@@ -18,34 +18,62 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-import ldap
-import re
-
-import core.users as users
-from core import Node
-import core.config as config
-import core.usergroups as usergroups
-
-import utils.date as date
 import logging
+import ldap
 
+from core import db, Node, User, UserGroup
 from core.auth import Authenticator
-
-ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-ldap.set_option(ldap.OPT_REFERRALS, 0)
+import core.config as config
+from core.auth import AuthenticatorInfo
+from utils.compat import iteritems
 
 
 logg = logging.getLogger(__name__)
 
+q = db.query
 
-def tryAuth(searchfilter):
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+ldap.set_option(ldap.OPT_REFERRALS, 0)
+
+LDAP_BASE_DN = config.get("ldap.basedn")
+LDAP_USER_LOGIN = config.get("ldap.user_login")
+LDAP_SERVER = config.get("ldap.server")
+LDAP_PROXYUSER = config.get("ldap.proxyuser")
+LDAP_PROXYUSER_PASSWORD = config.get("ldap.proxyuser_password")
+LDAP_USER_URL = config.get("ldap.user_url")
+LDAP_USER_LOGIN = config.get("ldap.user_login")
+LDAP_ATTRIBUTES = config.get("ldap.attributes").encode("utf8").split(",") + [LDAP_USER_LOGIN.encode("utf8")]
+LDAP_USER_GROUP = config.get("ldap.user_group")
+
+LDAP_USER_ATTRIBUTES = {
+    "lastname": config.get("ldap.user_lastname"),
+    "firstname": config.get("ldap.user_firstname"),
+    "email": config.get("ldap.user_email"),
+    "organisation": config.get("ldap.user_org"),
+    "comment": config.get("ldap.user_comment"),
+    "telephone": config.get("ldap.user_telephone"),
+    "login_name": LDAP_USER_LOGIN,
+    "display_name": config.get("ldap.user_displayname"),
+}
+
+if LDAP_USER_URL and LDAP_USER_URL.find("@") == -1:
+    LDAP_USER_URL = "@" + LDAP_USER_URL
+
+if "," not in LDAP_PROXYUSER:
+    BIND_USER = LDAP_PROXYUSER + LDAP_USER_URL
+else:
+    BIND_USER = LDAP_PROXYUSER
+
+
+def try_auth(searchfilter):
     count = 5
+    # if proxyuser given as full DN, take that
+    # otherwise assume cn as given and append configured basedn
     while True:
-        l = ldap.initialize(config.get("ldap.server"))
-        l.simple_bind_s(config.get("ldap.username"), config.get("ldap.password"))
+        l = ldap.initialize(LDAP_SERVER)
+        l.simple_bind_s(BIND_USER, LDAP_PROXYUSER_PASSWORD)
 
-        ldap_result_id = l.search(config.get("ldap.basedn"), ldap.SCOPE_SUBTREE, searchfilter, [config.get("ldap.user_login")])
+        ldap_result_id = l.search(LDAP_BASE_DN, ldap.SCOPE_SUBTREE, searchfilter, [])
         try:
             return l.result(ldap_result_id, 0, timeout=5)
 
@@ -59,16 +87,15 @@ def tryAuth(searchfilter):
             return None, None
 
 
-def tryLogin(user_dn, password, searchfilter):
+def try_login(user_dn, password, searchfilter):
     while True:
-        l2 = ldap.initialize(config.get("ldap.server"))
+        l2 = ldap.initialize(LDAP_SERVER)
         try:
             l2.simple_bind_s(user_dn, password)
         except ldap.INVALID_CREDENTIALS:
             return None, None
 
-        ldap_result_id = l2.search(config.get("ldap.basedn"), ldap.SCOPE_SUBTREE,
-                                   searchfilter, config.get("ldap.attributes").split(","))
+        ldap_result_id = l2.search(LDAP_BASE_DN, ldap.SCOPE_SUBTREE, searchfilter, LDAP_ATTRIBUTES)
         try:
             return l2.result(ldap_result_id, 0, timeout=5)
         except ldap.TIMEOUT:
@@ -78,139 +105,116 @@ def tryLogin(user_dn, password, searchfilter):
             return None, None
 
 
-def _getAttribute(self, attrname, list, separator=" "):
-    ret = ""
-    if ustr(attrname) == "":
-        return ""
-    for attr in ustr(attrname).split(","):
-        attr = attr.strip()
-        if attr in list.keys():
-            for i in list[attr]:
-                ret += i + separator
-    if ret.endswith(separator):
-        ret = ret[:(len(separator) * (-1))]
-    return ret.strip()
+def get_user_data(data):
+    return {attribute_name: data.get(ldap_fieldname)[0].decode("utf8") if data.get(ldap_fieldname) else None
+            for attribute_name, ldap_fieldname in iteritems(LDAP_USER_ATTRIBUTES)}
 
 
-def createLDAPUser(data, uname):
-    user = Node(uname, "user")
-    user.set("lastname", _getAttribute(config.get("ldap.user_lastname"), data))
-    user.set("firstname", _getAttribute(config.get("ldap.user_firstname"), data))
-    user.set("email", _getAttribute(config.get("ldap.user_email"), data))
-    user.set("organisation", _getAttribute(config.get("ldap.user_org"), data))
-    user.set("comment", _getAttribute(config.get("ldap.user_comment"), data))
-    user.set("identificator", _getAttribute(config.get("ldap.user_identificator"), data))
-    user.set("ldapuser.creationtime", date.format_date())
+def get_ldap_groups(data):
+    # TODO: handle multiple attributes
+    group_desc_list = data.get(LDAP_USER_GROUP)
+    if not group_desc_list:
+        return set()
 
-    if user.get("lastname") != "" and user.get("firstname") != "":
-        user.setName("%s %s" % (user.get("lastname"), user.get("firstname")))
+    if "," in group_desc_list[0]:
+        return set([group_dn.split(",")[0][3:] for group_dn in group_desc_list])
+    else:
+        return set(group_desc_list)
 
-    added_to_groups = 0
-    for group in _getAttribute(config.get("ldap.user_group"), data, ",").split(","):
-        if group != "" and not usergroups.existGroup(group):
-            #res = usergroups.create_group(group, description="LDAP Usergroup", option="")
-            #res.set("ldapusergroup.creationtime", date.format_date())
-            logg.info("skipped creation of ldap user group: ", group)
-            continue
-        g = usergroups.getGroup(group)
-        if g:
-            g.addChild(user)
-            added_to_groups += 1
 
-    logg.info("created ldap user: ", uname)
-    if not added_to_groups:
-        logg.warn("created ldap user %r, %r not added to any group", uname, user.id)
+def add_ldap_user(data, uname, authenticator_info):
+    s = db.session
+    user_data = get_user_data(data)
+    user = User(can_change_password=False, can_edit_shoppingbag=True, active=True, authenticator_info=authenticator_info, **user_data)
+    s.add(user)
+
+    if not user.display_name:
+        if user.lastname and user.firstname:
+            user.display_name = u"{} {}".format(user.lastname, user.firstname)
+
+    ldap_groups = get_ldap_groups(data)
+
+    for group_name in ldap_groups:
+        group = q(UserGroup).filter_by(name=group_name).scalar()
+        if group is not None:
+            user.groups.append(group)
+
+    s.commit()
+    logg.info("created ldap user: %s", uname)
     return user
 
 
-def updateLDAPUser(data, user):
-    if user.get("lastname") != _getAttribute(config.get("ldap.user_lastname"), data):
-        user.set("lastname", _getAttribute(config.get("ldap.user_lastname"), data))
+def update_ldap_user(data, user):
+    user_data = get_user_data(data)
+    user.update(**user_data)
 
-    if user.get("firstname") != _getAttribute(config.get("ldap.user_firstname"), data):
-        user.set("firstname", _getAttribute(config.get("ldap.user_firstname"), data))
+    ldap_groups = get_ldap_groups(data)
 
-    if user.get("email") != _getAttribute(config.get("ldap.user_email"), data) and user.get("email") == "":
-        user.set("email", _getAttribute(config.get("ldap.user_email"), data))
+    # check current groups if the are still in the list of group given by LDAP and remove missing ones
+    for group in user.groups:
+        # XXX: add 'automatic' attribute to user_to_usergroup table
+        if group.name not in ldap_groups and True:
+            user.groups.remove(group)
 
-    if user.get("organisation") != _getAttribute(config.get("ldap.user_org"), data):
-        user.set("organisation", _getAttribute(config.get("ldap.user_org"), data))
-
-    if user.get("comment") != _getAttribute(config.get("ldap.user_comment"), data):
-        user.set("comment", _getAttribute(config.get("ldap.user_comment"), data))
-
-    if user.get("identificator") != _getAttribute(config.get("ldap.user_identificator"), data):
-        user.set("identificator", _getAttribute(config.get("ldap.user_identificator"), data))
-
-    for group in _getAttribute(config.get("ldap.user_group"), data, ",").split(","):
-        if group != "" and not usergroups.existGroup(group):
-            logg.info("during ldap user update: skipped creation of ldap user group: ", group)
-            continue
-        g = usergroups.getGroup(group)
-        if g and g not in user.getParents():
-            g.addChild(user)
+    # add missing groups from LDAP if they are defined in our database
+    for group_name in ldap_groups:
+        group = q(UserGroup).filter_by(name=group_name).scalar()
+        if group is not None:
+            user.groups.append(group)
 
 
 class LDAPAuthenticator(Authenticator):
 
+    auth_type = u"ldap"
+
     def authenticate_user_credentials(self, login, password, request):
-        if login.find("@") == -1 and config.get("ldap.user_url", "") != "":
-            login += "@" + config.get("ldap.user_url", "")
+        if "@" not in login and LDAP_USER_URL:
+            login += LDAP_USER_URL
 
         searchfilter = config.get("ldap.searchfilter").replace("[username]", login)
 
-        result_type, auth_result_data = tryAuth(searchfilter)
+        result_type, auth_result_data = try_auth(searchfilter)
 
         if result_type == ldap.RES_SEARCH_RESULT:
             if len(auth_result_data) > 0:
                 result_type = ldap.RES_SEARCH_ENTRY
                 auth_result_data = auth_result_data[0]
             else:
-                return 0
+                return
 
         if result_type != ldap.RES_SEARCH_ENTRY:
-            return 0
+            return
 
         user_dn = auth_result_data[0][0]
+        auth_result_dict = auth_result_data[0][1]
+        dir_id = auth_result_dict[LDAP_USER_LOGIN][0]
 
-        result_type, login_result_data = tryLogin(user_dn, password, searchfilter)
+        result_type, login_result_data = try_login(user_dn, password, searchfilter)
 
         if (result_type == ldap.RES_SEARCH_RESULT and len(login_result_data) > 0):
             result_type = ldap.RES_SEARCH_ENTRY
-            result_data = login_result_data[0]
+            login_result_data = login_result_data[0]
         if (result_type != ldap.RES_SEARCH_ENTRY):
-            return 0
+            return
 
-        if result_data[0][0] == user_dn:
-            userfolder = users.getExternalUserFolder("ldapuser")
-            for user in userfolder.getChildren():
-                if user.getName() == login or login in user.get("identificator").split(" "):
-                    updateLDAPUser(result_data[0][1], user)  # update node information in mediatum
-                    return 1
+        if login_result_data[0][0] == user_dn:
+            user = q(User).filter_by(
+                login_name=dir_id).join(AuthenticatorInfo).filter_by(
+                name=self.name,
+                auth_type=LDAPAuthenticator.auth_type).scalar()
 
-            # check if newly to be created ldap user would be added to groups in function createLDAPUser
-            add_to_groups = 0
-            for group in self._getAttribute(config.get("ldap.user_group"), result_data[0][1], ",").split(","):
-                if group != "" and not usergroups.existGroup(group):
-                    continue
-                g = usergroups.getGroup(group)
-                if g:
-                    add_to_groups += 1
-
-            # refuse login if user would not be added to groups (such a user would not have any benefit from been loged in)
-            if not add_to_groups:
-                return 0
-
-            userfolder.addChild(createLDAPUser(result_data[0][1], login))  # add new user
-            return 1
-
-        return 0
-
-    def stdPassword(self, user):
-        return 0
-
-    def getName(self):
-        return "ldap user"
-
-    def canChangePWD(self):
-        return 0
+            if user is not None:
+                # we already have an user object, update data
+                update_ldap_user(login_result_data[0][1], user)
+                return user
+            else:
+                # check if newly to be created ldap user would be added to groups in function add_ldap_user
+                # refuse login if user would not be added to groups (such a user would not have any benefit from been logged in)
+                data = login_result_data[0][1]
+                for group_dn in data.get(config.get("ldap.user_group"), []):
+                    group_name = group_dn.split(",")[0][3:]
+                    group = q(UserGroup).filter_by(name=group_name).scalar() if group_name else None
+                    if group is not None:
+                        authenticator_info = q(AuthenticatorInfo).filter_by(name=self.name, auth_type=LDAPAuthenticator.auth_type).scalar()
+                        user = add_ldap_user(login_result_data[0][1], login, authenticator_info)
+                        return user

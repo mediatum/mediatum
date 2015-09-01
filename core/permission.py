e7bdd49399a18ab638b2965a92639feca0c6ef92
Tobias Stenzel <tobias.stenzel@tum.de>
2015-09-01 17:07:50 +0200

    added get_or_add_access_rule and get_or_add_everybody_rule helpers

---
+++ core/permission.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: (c) 2015 by the mediaTUM authors
+    :license: GPL3, see COPYING for details
+"""
+
+from core import AccessRule, db
+
+q = db.query
+
+
+def get_or_add_access_rule(group_ids=None, dateranges=None, subnets=None,
+                           invert_group=False, invert_date=False, invert_subnet=False):
+    """Fetches a access rule from the database or creates one if not found. The created rule is added to the session.
+    group_ids, dateranges and subnets default to None, which finds rules that don't have this value.
+    """
+
+    rule = q(AccessRule).filter_by(group_ids=group_ids, dateranges=dateranges, subnets=subnets,
+                                   invert_group=invert_group, invert_date=invert_date, invert_subnet=invert_subnet).first()
+
+    if rule is None:
+        rule = AccessRule(group_ids=group_ids, dateranges=dateranges, subnets=subnets,
+                          invert_group=invert_group, invert_date=invert_date, invert_subnet=invert_subnet)
+        db.session.add(rule)
+
+    return rule
+
+
+def get_or_add_everybody_rule():
+    return get_or_add_access_rule()

d46954eaf7c66b9c322bf600e0b248d9b4df0fe3
Tobias Stenzel <tobias.stenzel@tum.de>
2015-09-16 11:30:50 +0000

    migration: use low-level noderelation manipulation for home dirs
    * don't trigger the trigger...

--- migration/user_migration.py
+++ migration/user_migration.py
@@ -4,6 +4,7 @@
     :license: GPL3, see COPYING for details
 """
 import logging
+from sqlalchemy import text
 from core import db, Node, User
 from contenttypes import Directory, Home
 from core.translation import getDefaultLanguage, translate
@@ -17,6 +18,7 @@ def migrate_home_dirs(orphan_dir_id):
     """ Moves orphaned home dirs (not associated with an user) to a directory with id `orphan_dir_id`.
         The remaining home dirs are renamed: Arbeitsverzeichnis(<name>) -> <user_login_name>_<user_id>.
     """
+    s = db.session
     home_root = q(Home).one()
     orphan_dir = q(Directory).get(orphan_dir_id)
 
@@ -25,14 +27,20 @@ def migrate_home_dirs(orphan_dir_id):
         user = q(User).filter_by(home_dir_id=home_dir.id).first()
 
         if user is None:
-            logg.info("orphaned home dir %s (%s)", home_dir.name, home_dir.id)
+            logg.info("orphaned home dir name=%s id=%s", home_dir.name, home_dir.id)
             home_dir[u"system.migration"] = u"moved by mysql -> postgres migration"
-            home_root.children.remove(home_dir)
-            orphan_dir.container_children.append(home_dir)
-
+            # we go "low-level" here because we don't want to trigger noderelation updates via nodemapping
+            s.execute(text("DELETE FROM noderelation WHERE cid=:cid").bindparams(cid=home_dir.id))
+            s.execute(text("INSERT INTO noderelation VALUES(:nid, :cid, 1)").bindparams(nid=orphan_dir.id, cid=home_dir.id))
         else:
-            logg.info("home dir %s (%s) -> user login_name=%s (%s)", home_dir.name, home_dir.id, user.login_name, user.id)
-            home_dir.name = u"{}_{}".format(user.id, user.login_name)
+            new_name = u"{}_{}".format(user.id, user.login_name)
+            if home_dir.name == new_name:
+                logg.info("home dir name=%s id=%s already renamed", home_dir.name, home_dir.id)
+            else:
+                logg.info("renaming home dir name=%s id=%s associated with user login_name=%s id=%s",
+                          home_dir.name, home_dir.id, user.login_name, user.id)
+                home_dir.name = new_name
+
             home_dir[u"system.used_as"] = u"home"
 
 

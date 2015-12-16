c19b747de81384ff1cb68e421c520e4da63a2c4b
Tobias Stenzel <tobias.stenzel@tum.de>
2015-12-16 15:55:03 +0000

    fix xmlnode test (varying node id)

--- core/test/test_xmlnode.py
+++ core/test/test_xmlnode.py
@@ -10,19 +10,20 @@ from core.xmlnode import add_node_to_xmldoc, create_xml_nodelist
 from collections import OrderedDict
 
 expected_simple = '<nodelist exportversion="1.1a">'\
-+ '<node name="content" id="None" type="document/testschema" datatype="document" schema="testschema"><attribute name="sortattr"><![CDATA[6]]></attribute></node></nodelist>'
++ '<node name="content" id="111" type="document/testschema" datatype="document" schema="testschema"><attribute name="sortattr"><![CDATA[6]]></attribute></node></nodelist>'
 
 @fixture
 def nodelist():
     return create_xml_nodelist()
 
 
-def test_add_node_to_xmldoc_simple(nodelist, content_node):
+def test_add_node_to_xmldoc_simple(session, nodelist, content_node):
+    content_node.id = 111
     add_node_to_xmldoc(content_node, nodelist)
     assert etree.tostring(nodelist) == expected_simple
 
-def test_add_node_to_xmldoc(nodelist, parent_node):
 
+def test_add_node_to_xmldoc(nodelist, parent_node):
     add_node_to_xmldoc(parent_node, nodelist)
     xmlstr = etree.tostring(nodelist, pretty_print=True)
     # XXX: find a better way for XML assertions...

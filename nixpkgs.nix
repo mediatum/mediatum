2a3bc87f30a0caea6edeba0bf8e4a54aa9e2b953
Tobias Stenzel <tobias.stenzel@tum.de>
2016-03-08 16:27:53 +0000

    update nixpkgs version to nixos-16.03.30.2068621

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,1 +1,1 @@
-fetchTarball https://nixos.org/releases/nixos/unstable/nixos-16.03pre77568.40c586b/nixexprs.tar.bz2 
+fetchTarball https://nixos.org/releases/nixos/16.03-beta/nixos-16.03.30.2068621/nixexprs.tar.xz 

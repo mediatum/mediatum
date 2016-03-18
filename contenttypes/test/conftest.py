28f2ecf7c3b995a06cb7f869bb4251afe41e3cac
Tobias Stenzel <tobias.stenzel@tum.de>
2016-03-18 15:54:13 +0000

    fix test_show_node_big_video_url

---
+++ contenttypes/test/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: (c) 2016 by the mediaTUM authors
+    :license: GPL3, see COPYING for details
+"""
+from mediatumtal import tal
+from web.frontend.test.fixtures import req
+from core import config
+
+tal.set_base(config.basedir)

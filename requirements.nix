b8944f46fa04b94d27109295807fd0bfa70058ea
Wolfgang Boiger <wolfgang.boiger@ub.tum.de>
2019-02-01 12:35:33 +0100

    Patch WTForms 2.2.1 to restore compatibility with SQLAlchemy 1.2+, closes #2343
    WTForms calls a function from SQLAlchemy (if present).
    But the return value of that function changed
    with SQLAlchemy 1.2; WTForms gets more
    than it asks for and an exception occurs.
    
    Sadly, as of this moment, there is no new release
    of WTForms that fixes the error, but it is fixed
    in the current master branch in Github.
    We import the patch from Github and
    inject it into WTForms build script.

--- requirements.nix
+++ requirements.nix
@@ -1,4 +1,12 @@
-{fetchurl, fetchgit, fetchFromGitHub, fetchhg, stdenv, self, pkgs}:
+{ pkgs
+, fetchFromGitHub
+, fetchgit
+, fetchhg
+, fetchpatch
+, fetchurl
+, self
+, stdenv
+}:
 
 let
   pythonPackages = pkgs.python27Packages;
@@ -231,6 +239,13 @@ let
       url = "https://pypi.python.org/packages/source/W/WTForms/WTForms-2.2.1.zip";
       sha256 = "0vyl26y9cg409cfyj8rhqxazsdnd0jipgjw06civhrd53yyi1pzz";
     };
+    patches = [
+      # old wtforms seems to be unable to cope with new SQLAlchemy
+      (fetchpatch {
+        url = "https://github.com/wtforms/wtforms/commit/6cc9e87b9d22baa37a0570ce901b094170270ab4.patch";
+        sha256 = "089xlw25qlsb6kz6gzl8rwwj1gx8nkhn3jgp4f0b8f0m5da2klyi";
+      })
+    ];
   };
 
   dogpile_cache = self.buildPythonPackage {

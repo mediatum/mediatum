123c5bd32f81307b7b8e6f79973a620e9b8a54a9
Nikolaus Kolbenschlag <nikolaus.kolbenschlag@tum.de>
2019-02-01 08:17:52 +0100

    fts: create new primary key, closes #2173
    The current primary key id for the table fts makes an update of the table node very slow because the trigger update_node_tsvector must do an seqential scan of the table fts to find node id nid for the node which is updated.
    So a better primary key is the combination nid,config,searchtype.
    For changing the primary key a new alembic version must be created.

---
+++ migration/versions/e747587ee8a1_fts_new_primary_key.py
@@ -0,0 +1,29 @@
+"""fts new primary key
+
+Revision ID: e747587ee8a1
+Revises: 4df503937ab1
+Create Date: 2018-07-23 07:09:17.595063
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'e747587ee8a1'
+down_revision = '4df503937ab1'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    # Drop primary key constraint. Note the CASCASE clause - this deletes the foreign key constraint.
+    op.execute('ALTER TABLE fts DROP CONSTRAINT fts_pkey CASCADE')
+    # Re-create the primary key constraint
+    op.create_primary_key('fts_pkey', 'fts', ['nid','config','searchtype'])
+
+def downgrade():
+    # Drop primary key constraint. Note the CASCASE clause - this deletes the foreign key constraint.
+    op.execute('ALTER TABLE fts DROP CONSTRAINT fts_pkey CASCADE')
+    # Re-create the primary key constraint
+    op.create_primary_key('fts_pkey', 'fts', ['id'])

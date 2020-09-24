23c8797a24f42dc90f34cc3421de21a20bf3ae3a
congkhacdung <dung.cong-khac@ub.tum.de>
2020-09-24 05:47:55 +0000

    bin/mediatum-z3950.py: write pid to file (#2813)
    write pid to file following the configuration directive paths.pidfile

--- bin/mediatum-z3950.py
+++ bin/mediatum-z3950.py
@@ -4,6 +4,7 @@ _sys.path.append(_os.path.abspath(_os.path.join(_os.path.dirname(__file__), ".."
 import asyncore as _asyncore
 import configargparse as _configargparse
 import core.init as _core_init
+import core.config as _core_config
 import core.athana_z3950 as _core_athana_z3950
 import utils.utils as _utils_utils
 
@@ -16,6 +17,10 @@ def run():
     parser.add_argument("-l", "--loglevel", help="root loglevel, sensible values: DEBUG, INFO, WARN")
     args = parser.parse_args()
     _core_init.full_init(force_test_db=args.force_test_db, root_loglevel=args.loglevel)
+
+    with open(_core_config.get("paths.pidfile"), "wb") as f:
+        f.write(str(_os.getpid()))
+
     _core_athana_z3950.z3950_server(port=args.port)
     while True:
         with _utils_utils.suppress(Exception, warn=False):

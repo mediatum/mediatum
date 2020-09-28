559d1bb0550aec26113ad804c0270895bbf258b9
congkhacdung <dung.cong-khac@ub.tum.de>
2020-09-28 05:34:54 +0000

    uri in athana: replaced by flask path (#2814)
    they were request.uri in athana, now replaced by flask.request.full_path.

--- web/frontend/filehandlers.py
+++ web/frontend/filehandlers.py
@@ -412,7 +412,7 @@ def send_from_webroot(req):
 ### redirects for legacy handlers
 
 def redirect_images(req):
-    req.response.headers["Location"] = "/image" + req.mediatum_contextfree_path[7:]
+    req.response.headers["Location"] = "/image" + req.full_path[7:]
     req.response.status_code = 301
     return 301
 

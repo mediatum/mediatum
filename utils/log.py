b1f4a534e53f98c73fc929f1d62ff90f5f2fe625
congkhacdung <dung.cong-khac@ub.tum.de>
2020-10-28 08:41:01 +0000

    utils/log.py: change to WatchedFileHandler (#2813)
    user logging.handlers.WatchedFileHandler to enable reopen file if the file is moved
    because of usage of programs such as newsyslog and logrotate.

--- utils/log.py
+++ utils/log.py
@@ -317,7 +317,7 @@ def initialize(level=None, log_filepath=None, log_filename=None, use_logstash=No
 
     if log_filepath:
         dlogfiles['mediatum'] = {'path': log_filepath, 'filename': log_filepath}
-        file_handler = logging.FileHandler(log_filepath)
+        file_handler = logging.handlers.WatchedFileHandler(log_filepath)
         file_handler.setFormatter(logging.Formatter(ROOT_FILE_LOGFORMAT))
         root_logger.addHandler(file_handler)
         logg.info('--- logging everything to %s ---', log_filepath)

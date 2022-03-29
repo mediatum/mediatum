a40de453da01e4ec42415c97a572ca31694617be
Wolfgang Boiger <wolfgang.boiger@ub.tum.de>
2022-03-29 10:32:47 +0200

    web.frontend.search: simplify `_extended_searchquery_from_req` (#2940)
    Turn the internal function into a generator.
    Replace some string concatenation with lists.
    Drop unneeded variables.

--- web/frontend/search.py
+++ web/frontend/search.py
@@ -4,6 +4,8 @@
 from __future__ import division
 from __future__ import print_function
 
+import itertools as _itertools
+
 import utils.date as date
 import logging
 
@@ -119,98 +121,83 @@ def simple_search(req, paths, container_id=None):
 
 
 def _extended_searchquery_from_req(req):
-    max_fields = 3
-    if req.args.get("searchmode") == "extendedsuper":
-        max_fields = 10
-
-    q_str = u''
-    q_user = u''
-    first2 = 1
-    for i in range(1, max_fields + 1):
-        query_key = "query" + unicode(i)
+    for i in range(1, 11 if req.args.get("searchmode") == "extendedsuper" else 4):
         # for range queries
-        query_to_key = "query" + unicode(i) + "-to"
-        query_from_key = "query" + unicode(i) + "-from"
-        field_id_or_name = req.args.get("field" + unicode(i), "").strip()
-        element_query = req.args.get(query_key, "").strip()
+        query_to_key = u"query{}-to".format(unicode(i))
+        query_from_key = u"query{}-from".format(unicode(i))
+        field_id_or_name = req.args.get(u"field{}".format(unicode(i)), "").strip()
+        element_query = req.args.get(u"query{}".format(unicode(i)), "").strip()
 
         if not element_query and query_from_key not in req.args:
-            # no query found, do nothing
-            continue
-
-        if not first2:
-            q_str += " and "
-            q_user += "{} ".format(_core_translation.translate("search_and", request=req))
-
-        first2 = 0
+            continue  # no query found, do nothing
 
         if not field_id_or_name.isdigit():
-            q_str += field_id_or_name + '=' + protect(element_query)
-            q_user += field_id_or_name + '=' + protect(element_query)
-        else:
-            searchmaskitem = q(SearchMaskItem).get(field_id_or_name)
-            first = 1
-            q_str += "("
-            for field in searchmaskitem.children:
-                if not first:
-                    q_str += " or "
-                    q_user += " {} ".format(_core_translation.translate("search_or", request=req))
-                first = 0
-                field_type = field.getFieldtype()
-                if query_to_key in req.args and field_type == "date":
-                    date_from = "0000-00-00T00:00:00"
-                    date_to = "0000-00-00T00:00:00"
+            yield (
+                    u"{} = {}".format(field_id_or_name, protect(element_query)),
+                    u"{} = {}".format(field_id_or_name, protect(element_query)),
+                   )
+            continue
 
-                    from_value = req.args.get(query_from_key)
-                    if from_value:
-                        try:
-                            date_from = date.format_date(date.parse_date(from_value, field.getValues()), "%Y-%m-%dT%H:%M:%S")
-                        except ValueError:
-                            date_from = "9999-00-00T00:00:00"
-
-                    to_value = req.args.get(query_to_key)
-                    if to_value:
-                        try:
-                            date_to = date.format_date(date.parse_date(to_value, field.getValues()), "%Y-%m-%dT%H:%M:%S")
-                        except ValueError:
-                            date_from = "0000-00-00T00:00:00"
-
-                    if date_from == "0000-00-00T00:00:00" and date_to != date_from:
-                        q_str += field.name + ' <= ' + date_to
-                        q_user += "%s &le; \"%s\"" % (field.name, to_value)
-
-                    elif date_to == "0000-00-00T00:00:00" and date_to != date_from:
-                        q_str += field.name + ' >= ' + date_from
-                        q_user += "%s &ge; \"%s\"" % (field.name, from_value)
-                    else:
-                        q_str += u'({} >= {} and {} <= {})'.format(field.name, date_from, field.name, date_to)
-
-                        q_user += u"({} {} \"{}\" {} \"{}\")".format(
-                                field.name,
-                                _core_translation.translate("search_between", request=req),
-                                from_value,
-                                _core_translation.translate("search_and", request=req),
-                                to_value,
-                            )
+        q_str = list()
+        q_user = list()
+        for field in q(SearchMaskItem).get(field_id_or_name).children:
+            field_type = field.getFieldtype()
+            if not (query_to_key in req.args and field_type == "date"):
+                if field_type == "number":
+                    # exact match
+                    q_str.append(u"{} eq {}".format(field.name, protect(element_query)))
                 else:
-                    # XXX: what about dates?
-                    if field_type == "number":
-                        # exact match
-                        q_str += field.name + " eq " + protect(element_query)
-                    else:
-                        # fulltext match
-                        q_str += field.name + " = " + protect(element_query)
+                    # fulltext match
+                    q_str.append(u"{} = {}".format(field.name, protect(element_query)))
 
-                    if field.label:
-                        q_user += field.label + " = " + protect(element_query)
-                    else:
-                        q_user += field.name + " = " + protect(element_query)
+                if field.label:
+                    q_user.append(u"{} = {}".format(field.label, protect(element_query)))
+                else:
+                    q_user.append(u"{} = {}".format(field.name, protect(element_query)))
+                continue
 
-            q_str += ")"
+            date_from = "0000-00-00T00:00:00"
+            date_to = "0000-00-00T00:00:00"
 
-    return q_str, q_user.strip()
+            from_value = req.args.get(query_from_key)
+            if from_value:
+                try:
+                    date_from = date.format_date(date.parse_date(from_value, field.getValues()), "%Y-%m-%dT%H:%M:%S")
+                except ValueError:
+                    date_from = "9999-00-00T00:00:00"
+
+            to_value = req.args.get(query_to_key)
+            if to_value:
+                try:
+                    date_to = date.format_date(date.parse_date(to_value, field.getValues()), "%Y-%m-%dT%H:%M:%S")
+                except ValueError:
+                    date_from = "0000-00-00T00:00:00"
+
+            if date_from == "0000-00-00T00:00:00" and date_to != date_from:
+                q_str.append(u"{} <= {}".format(field.name, date_to))
+                q_user.append(u'{} &le; "{}"'.format(field.name, to_value))
+
+            elif date_to == "0000-00-00T00:00:00" and date_to != date_from:
+                q_str.append(u"{} >= {}".format(field.name, date_from))
+                q_user.append(u'{} &ge; "{}"'.format(field.name, from_value))
+            else:
+                q_str.append(u'({} >= {} and {} <= {})'.format(field.name, date_from, field.name, date_to))
+                q_user.append(u'({} {} "{}" {} "{}")'.format(
+                        field.name,
+                        _core_translation.translate("search_between", request=req),
+                        from_value,
+                        _core_translation.translate("search_and", request=req),
+                        to_value,
+                       ))
+
+        yield (
+                u"({})".format(" or ".join(q_str)),
+                u" {} ".format(_core_translation.translate("search_or", request=req)).join(q_user),
+               )
 
 
 def extended_search(req, paths):
-    searchquery, readable_query = _extended_searchquery_from_req(req)
+    searchquery, readable_query = _itertools.izip(*_extended_searchquery_from_req(req))
+    searchquery =  " and ".join(searchquery)
+    readable_query = " {} ".format(_core_translation.translate("search_and", request=req)).join(readable_query)
     return search("extended", searchquery, readable_query, paths, req)

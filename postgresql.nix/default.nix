344c3844d0c951d42a4be3995d1e6e25a96ecee0
Nikolaus Kolbenschlag <nikolaus.kolbenschlag@tum.de>
2022-04-08 05:54:02 +0200

    postgresql.nix/default.nix: postgres version 14.1 (#3067)
    Switch from postgres major version 10 to 14
    
    https://www.postgresql.org/docs/14/release-14-1.html
    
    To switch from postgres 10 to postgres 14 the following steps are
    neccessary:
    
    before switching to postgres 14 create a database dump in nix-shell:
    
    - pg_dump --format=custom --compress=0 --dbname=mediatum --host=%h/postgresql-sock --username=mediatum --no-password > <dbdumpfile>
    
    after switching to postgres 14 start the nix-shell and create a new database directory <dbdir> and initialize it
    
    - mkdir <dbdir>
    - initdb --pgdata=<dbdir> --encoding=UTF8 --no-locale --username=postgres --auth-host=md5 --auth-local=trust
    - copy postgresql.conf from old dbdir to the new dbdir and adopt it
    
    start postgres
    - pg_ctl start -D <dbdir>
    
    start psql and execute the commands:
    - psql -h $HOME/postgresql.sock -U postgres
    CREATE USER mediatum;
    CREATE DATABASE mediatum OWNER mediatum;
    CREATE EXTENSION hstore schema public;
    
    leave pg_ctl and restore the previously created databasedump <dbdumpfile>:
    - pg_restore --host=$HOME/postgresql.sock --username=postgres --format=custom --dbname=mediatum --jobs=8 <dbdumpfile>

--- postgresql.nix/default.nix
+++ postgresql.nix/default.nix
@@ -1,9 +1,9 @@
 { pkgs ? (import ../nixpkgs.nix {}).pkgs
 , lib ? pkgs.lib
-, postgresql_10 ? pkgs.postgresql_10
+, postgresql_14 ? pkgs.postgresql_14
 }:
 
-postgresql_10.overrideAttrs ( oldAttrs: {
+postgresql_14.overrideAttrs ( oldAttrs: {
   postInstall = oldAttrs.postInstall + ''
     install --verbose -D --mode=0644 --no-target-directory "${./unaccent_german_umlauts_special.rules}" "$out/share/postgresql/tsearch_data/unaccent_german_umlauts_special.rules"
   '';

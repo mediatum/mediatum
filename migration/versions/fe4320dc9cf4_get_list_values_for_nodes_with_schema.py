c8e5bac6ec96710c4d2e3c204cac89ec4e6904ad
congkhacdung <congkhacdung@yahoo.de>
2022-06-02 12:55:27 +0200

    fe4320dc9cf4_get_list...: consolidate license text (#1475)
    change to actual license text.

--- migration/versions/fe4320dc9cf4_get_list_values_for_nodes_with_schema.py
+++ migration/versions/fe4320dc9cf4_get_list_values_for_nodes_with_schema.py
@@ -1,5 +1,6 @@
 # Copyright (C) since 2007, Technical University of Munich (TUM) and mediaTUM authors
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 """drop get_list_values_for_nodes_with_schema
 
 Revision ID: fe4320dc9cf4

8c4212e7e4f6cb547d5f32a521739276a4ff8f08
congkhacdung <dung.cong-khac@ub.tum.de>
2022-12-06 09:53:33 +0100

    web/frontend/printview.py: handle MessageIdNotFound (#3227)
    return collection.name if it is not defined in po files (#3063) that is
    missed in commit d6ed7e9c "core/translation.py: restructure translate function (#3063)".

--- web/frontend/printview.py
+++ web/frontend/printview.py
@@ -85,7 +85,11 @@ class PrintPreview:
         if not collection:
             self.header = Paragraph(_core_translation.translate(self.language, "print_view_header"), h1)
         else:
-            p = Paragraph(_core_translation.translate(self.language, collection.name), h1)
+            try:
+                collection_name = _core_translation.translate(self.language, collection.name)
+            except _core_translation.MessageIdNotFound:
+                collection_name = collection.name
+            p = Paragraph(collection_name, h1)
             p.wrap(defaultPageSize[0], defaultPageSize[1])
             self.headerWidth = p.getActualLineWidths0()[0]
             self.header = p

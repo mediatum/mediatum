a8fed830a71fcbc437573cba6169044b5526c5a9
congkhacdung <dung.cong-khac@ub.tum.de>
2022-12-15 13:28:11 +0100

    web/.../macros.j2.jade: move javascript function (#3074)
    move insertParameter(k, v) from macros.j2.jade to mediatum.js.

--- static/js/mediatum.js
+++ static/js/mediatum.js
@@ -182,3 +182,28 @@ function handleArchived(nodeid, filename){
         return unescape(data);
     });
 }
+
+function insertParameter(k, v) {
+    // add a parameter to the url
+    key = encodeURIComponent(k);
+    value = encodeURIComponent(v);
+    var kvp = document.location.search.substr(1).split('&');
+    if (kvp == '') {
+        document.location.search = '?' + key + '=' + value;
+    } else {
+        var i = kvp.length;
+        var x;
+        while (i--) {
+            x = kvp[i].split('=');
+            if (x[0] == key) {
+                x[1] = value;
+                kvp[i] = x.join('=');
+                break;
+            }
+        }
+        if (i < 0) {
+            kvp[kvp.length] = [key, value].join('=');
+        }
+        document.location.search = kvp.join('&');
+    }
+}

01e7cab8c015bd6f5f1106b6d346c96ece87dca6
Wolfgang Boiger <wolfgang.boiger@ub.tum.de>
2023-01-03 12:13:53 +0100

    metadata/list: handle list structures as JSON/YAML (#3223)
    This changes the way the admin-predefined list for
    a "list" metadata field is stored in the database.
    
    The old way was a huge and newline-separated text.
    Each line defined one element, or an element-"headline" which
    is unselectable but permit to create a hierarchical structure).
    To create headline element, lines are prefixed with asterisks.
    
    Here are singular examples
    (each line is not connected to other lines)
    
                indent   selectable   final_value
     --------------------------------------------
     "A", " A"       0            1           "A"
     " *A"           0            1          "*A"
     "*A"            0            0           "A"
     "* A"           0            1           "A"
     "**A"           1            0           "A"
     "** A"          1            1           "A"
     "***A"          2            0           "A"
     "*** A"         2            1           "A"
    
    Here is a more involved example
    (square brackets mean not selectable)
    
    a               - a
    b               - b
     *              - "*"
     **             - "**"
     ***            - "***"
    *A              - [A]
    * AA              - AA  # indent with same star-count
    * AB              - AB  # creates subgroup
    *B              - [B]
    ** BA             - BA  # indent with another star
    ** BB             - BB  # creates subgroup as well
    *C              - [C]
    **CA              - [CA]
    ***CAA              - [CAA]
    *** CAAA              - CAAA
    ***CAB              - [CAB]
    ****CABA              - [CABA]
    **** CABAA              - CABAA
    * D             - D  # unclear (maybe not important!)
    *E              - [E]
    **EA              - [EA]
    ***EAA              - [EAA]
    *** EAAA              - EAAA
    *** EAAB              - EAAB
    **** EAACA              - EAACA
    **** EAACB              - EAACB
    ****EAACE               - [EAACE]
    **** EAACEA               - EAACEA
    **** EAACEB               - EAACEB
    *F              - [F]
    * FA              - FA
    * FB              - FB
    **FBA               - [FBA]
    ** FBAA               - FBAA
    ** FBAB               - FBAB
    ***FBACA                - [FBACA]
    *** FBACAA                - FBACAA
    *** FBACAB                - FBACAB
    ***FBACB                - [FBACB]
    *** FBACBA                - FBACBA
    *** FBACBB                - FBACBB
    **** FBACBCA                - FBACBCA
    **** FBACBCB                - FBACBCB
    *****FBACBCCA                 - [FBACBCCA]
    ***** FBACBCCAA                 - FBACBCCAA
    ***** FBACBCCAB                 - FBACBCCAB
    ***FBACB                - [FBACB]
    FBACBA                    - FBACBA
    FBACBB                    - FBACBB
    ***FBACC                - [FBACC]
    *** FBACCA                - FBACCA
    *G              - [G]
    **GA              - [GA]
    ***GAA              - [GAA]
    
    The new structure represents the data as JSON-structure
    in the database and communicates it with the admin via YAML.
    The structure recreates the hierarchy:
    
     - El1
     - El2
     - name: El3
       selectable: true
       subelements:
         - Sub1
         - Sub2
         - name: Sub3
           selectable: false
           subelements:
             - subsub1
             - subsub2

--- metadata/list.py
+++ metadata/list.py
@@ -4,9 +4,11 @@
 from __future__ import division
 from __future__ import print_function
 
-import logging
+import collections as _collections
+import cStringIO as _cStringIO
 
 from mediatumtal import tal
+import ruamel.yaml as _ruamel_yaml
 
 from utils.utils import esc
 from core.metatype import Metatype
@@ -14,11 +16,32 @@ from core import db
 import metadata.common_list as _common_list
 
 q = db.query
-logg = logging.getLogger(__name__)
 
+_Element = _collections.namedtuple("_Element", "label optgroup indent count selected");
+# label: just the name from the elements list
+# indent: number of spaces before the label
+# counts: number of occurences in the data base (to be displayed in parentheses)
+# optgroup: True/False
+# selected: True/False
 
-def _format_elements(field, *args):
-    return _common_list.format_elements(field.metatype_data["listelements"], field, *args)
+def _format_elements(elements, selected_elements=frozenset(), counts={}, _indent=0):
+    for element in elements:
+        if not isinstance(element, _collections.Mapping):
+            assert element >= ""
+            element = dict(
+                name=element,
+                selectable=True,
+                subelements=(),
+               )
+        yield _Element(
+                count=counts.get(element["name"]) if element["selectable"] else None,
+                indent=_indent,
+                label=element["name"],
+                optgroup=not element["selectable"],
+                selected=element["selectable"] and (element["name"] in selected_elements),
+               )
+        for e in _format_elements(element["subelements"], selected_elements, counts, _indent=_indent+1):
+            yield e
 
 
 class m_list(Metatype):
@@ -31,9 +54,9 @@ class m_list(Metatype):
     )
 
     def get_default_value(self, field):
-        valuelist = next(_format_elements(field))
-        if valuelist.opt in ("option", "optionselected"):
-            return valuelist.item
+        element = next(_format_elements(field.metatype_data["listelements"]))
+        if not element.optgroup:
+            return element.label
 
     def editor_get_html_form(self, field, value="", width=400, lock=0, language=None, required=None):
         return tal.getTAL(
@@ -44,7 +67,10 @@ class m_list(Metatype):
                     multiple=field.metatype_data['multiple'],
                     name=field.getName(),
                     required=1 if required else None,
-                    valuelist=_format_elements(field, value.split(";")),
+                    elements=tuple(_format_elements(
+                            field.metatype_data["listelements"],
+                            selected_elements=frozenset(value.split(";")),
+                           )),
                     width=width,
                   ),
                 macro="editorfield",
@@ -56,8 +82,14 @@ class m_list(Metatype):
                 "metadata/list.html",
                 dict(
                     name=name,
-                    value=value,
-                    valuelist=_format_elements(field, value, collection),
+                    elements=tuple(_format_elements(
+                            field.metatype_data["listelements"],
+                            selected_elements=frozenset((value,)),
+                            counts=dict(_common_list.count_list_values_for_all_content_children(
+                                    collection.id,
+                                    field.getName(),
+                                   )),
+                           )),
                    ),
                 macro="searchfield",
                 language=language,
@@ -78,11 +110,16 @@ class m_list(Metatype):
         return value.replace("; ", ";")
 
     def admin_settings_get_html_form(self, fielddata, metadatatype, language):
+        output = _cStringIO.StringIO()
+        yaml = _ruamel_yaml.YAML(typ="safe", pure=True)
+        yaml.indent(mapping=2, sequence=4, offset=2)
+        yaml.default_flow_style = False
+        yaml.dump(fielddata['listelements'], output)
         return tal.getTAL(
             "metadata/list.html",
             dict(
                     multiple_list=fielddata['multiple'],
-                    value=u"\r\n".join(fielddata['listelements']),
+                    value=output.getvalue().replace('\n', '\r\n'),
                ),
             macro="metafieldeditor",
             language=language,
@@ -90,7 +127,7 @@ class m_list(Metatype):
 
     def admin_settings_parse_form_data(self, data):
         if "listelements" in data:
-            listelements = data["listelements"].split("\r\n")
+            listelements = _ruamel_yaml.YAML(typ="safe", pure=True).load(data["listelements"])
         else:
             listelements = ()
 
---
+++ migration/versions/81a3d17301ab_metatype_list_to_yaml_format.py
@@ -0,0 +1,149 @@
+# Copyright (C) since 2007, Technical University of Munich (TUM) and mediaTUM authors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+"""metatype_list_to_yaml_format
+
+Revision ID: 81a3d17301ab
+Revises: ed88eef38764
+Create Date: 2023-01-05 05:56:11.303235
+
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from itertools import imap as map
+from itertools import ifilter as filter
+range = xrange
+
+import collections as _collections
+import os as _os
+import sys as _sys
+
+_sys.path.append(_os.path.abspath(_os.path.join(_os.path.dirname(__file__), "../..")))
+
+import core as _core
+import core.init as _core_init
+_core_init.full_init()
+import schema.schema as _schema
+
+
+# revision identifiers, used by Alembic.
+revision = '81a3d17301ab'
+down_revision = u'ed88eef38764'
+branch_labels = None
+depends_on = None
+
+_q = _core.db.query
+
+
+def _parse_item(item):
+    element = dict(subelements=list())
+
+    # count and strip prefix stars
+    element["stars"] = len(item)
+    item = item.lstrip("*")
+    element["stars"] -= len(item)
+
+    # detect and strip prefix space
+    element["name"] = item.lstrip(" ")
+    element["selectable"] = bool(item.startswith(" ") or not element["stars"])
+
+    return element
+
+
+def _clean_elements(elements):
+    # clean "internal" data "stars"
+    # turn simple elements into strings (selectable, no subelements)
+    for element in elements:
+        if element["selectable"] and not element["subelements"]:
+            yield element["name"]
+            continue
+        del element["stars"]
+        element["subelements"] = tuple(_clean_elements(element["subelements"]))
+        yield element
+
+
+def _upgrade_items(items):
+
+    # path is a list of element structures:
+    # its last element is the previously inserted one,
+    # the elements before at the parents of the previously inserted one.
+    # the first element is a top-level dummy element;
+    # its subelements will be the list that is returned as real top-level structure
+    path = [dict(stars=-1, subelements=list())]
+
+    for element in map(_parse_item, items):
+
+        # for the candidate `element` for insertion,
+        # we go the path upwards from the lowest element to find
+        # a suitable parent for the new `element`
+        while True:
+            parent = path.pop()
+
+            # special case: if the candidate `element` has no stars,
+            # it should be attached the next possible
+            # previous parent if that one has stars
+            if parent["stars"] > 0 and not element["stars"]:
+                break
+
+            # (still the same) special case:
+            # if the parent element has no stars, but the `element`
+            # does, the parent is actually one level *below*
+            # the current element, and we have to skip it
+            # (and likely also the next candidate)
+            if element["stars"] and not parent["stars"]:
+                continue
+
+            # if a parent candidate has more stars than the `element`,
+            # it can never be a parent of `element`
+            if parent["stars"] > element["stars"]:
+                continue
+
+            # if a parent candidate has fewer stars than the `element`,
+            # it certainly is the correct parent
+            # (this will always accept the top-level dummy as
+            # parent if all other elements are exhausted)
+            if parent["stars"] < element["stars"]:
+                break
+
+            # the parent candidate has the same amount of stars as the `element`:
+            # it can only be the real parent it is not selectable, while the `element` is
+            if element["selectable"] and not parent["selectable"]:
+                break
+
+        # attach element to the parent
+        parent["subelements"].append(element)
+        path.extend((parent, element))
+
+    return tuple(_clean_elements(path[0]["subelements"]))
+
+
+def upgrade():
+    for metafield in _q(_schema.Metafield).filter(_schema.Metafield.a.type == 'list').prefetch_attrs():
+        data = metafield.metatype_data
+        data['listelements'] = _upgrade_items(data['listelements'])
+        metafield.metatype_data = data
+    _core.db.session.commit()
+
+
+def _downgrade_element(elements, _indent=1):
+    for element in elements:
+        if not isinstance(element, _collections.Mapping):
+            assert element >= ""
+            element = dict(
+                name=element,
+                selectable=True,
+                subelements=(),
+               )
+        yield u"{}{}{}".format("*"*_indent, ' ' if element["selectable"] else '', element["name"])
+        for e in _downgrade_element(element["subelements"], _indent + 1):
+            yield e
+
+
+def downgrade():
+    for metafield in _q(_schema.Metafield).filter(_schema.Metafield.a.type == 'list').prefetch_attrs():
+        data = metafield.metatype_data
+        data['listelements'] = tuple(_downgrade_element(data['listelements']))
+        metafield.metatype_data = data
+    _core.db.session.commit()

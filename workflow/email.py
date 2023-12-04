c221021e3057ed1ba17a89f13a6fe79126453617
congkhacdung <dung.cong-khac@ub.tum.de>
2023-12-04 07:41:40 +0100

    workflow/email.py: new attributes for email (#3341)

--- workflow/email.py
+++ workflow/email.py
@@ -34,9 +34,13 @@ class WorkflowStep_SendEmail(WorkflowStep):
         allowedit=False,
         attach_pdf_form=False,
         recipient=(),
-        sender="",
-        subject="",
-        text="",
+        from_email="",
+        from_name=None,
+        from_envelope=None,
+        reply_to_email=None,
+        reply_to_name=None,
+        subject=None,
+        text=None,
     )
 
     def get_tal_renderer(self, node, language):
@@ -49,11 +53,11 @@ class WorkflowStep_SendEmail(WorkflowStep):
             context["language"] = language
 
         def renderer(text):
-            return _tal.getTALstr(text, context).replace('\n', '').strip()
+            return None if text is None else _tal.getTALstr(text, context).replace('\n', '').strip()
 
         return renderer
 
-    def send_email(self, node, sender, recipients, subject, text):
+    def send_email(self, node, sender, recipients, subject, text, envelope_sender_address=None, reply_to=None):
         try:
             if not recipients:
                 raise RuntimeError("No receiver address defined")
@@ -63,7 +67,7 @@ class WorkflowStep_SendEmail(WorkflowStep):
                 attachments = {f.abspath.split("_")[-1]:f.abspath for f in node.files if f.filetype=="wfstep-addformpage"}
             else:
                 attachments = {}
-            mail.sendmail(sender, recipients, subject, text, attachments=attachments)
+            mail.sendmail(sender, recipients, subject, text, envelope_sender_address, attachments=attachments, reply_to=reply_to)
         except:
             logg.exception("Error while sending mail- node stays in workflowstep %s %s", self.id, self.name)
             raise
@@ -71,18 +75,25 @@ class WorkflowStep_SendEmail(WorkflowStep):
     def runAction(self, node, op=""):
         if self.settings["allowedit"]:
             return
+
         tal_renderer = self.get_tal_renderer(node, node.get("system.wflanguage"))
-        sender = tal_renderer(self.settings['sender'])
+
+        from_envelope = tal_renderer(self.settings["from_envelope"])
+        from_email = mail.EmailAddress(tal_renderer(self.settings["from_email"]), tal_renderer(self.settings["from_name"]))
         recipients = map(tal_renderer, self.settings['recipient'])
         subject = tal_renderer(self.settings["subject"])
         text = tal_renderer(self.settings["text"])
+        reply_to_email = tal_renderer(self.settings["reply_to_email"])
+        reply_to_name = tal_renderer(self.settings["reply_to_name"])
         try:
             self.send_email(
                 node,
-                mail.EmailAddress(sender, None),
+                from_email,
                 tuple(mail.EmailAddress(r, None) for r in recipients),
                 subject,
                 text,
+                from_envelope,
+                mail.EmailAddress(reply_to_email, reply_to_name) if reply_to_email else None,
             )
         except:
             # Will raise later in 'show_workflow_node'
@@ -98,21 +109,33 @@ class WorkflowStep_SendEmail(WorkflowStep):
                 node, node.get("system.wflanguage") or _core_translation.set_language(req.accept_languages),
             )
 
-        sender = req.values["sender"] if "sender" in req.values else tal_renderer(self.settings['sender'])
-        recipient = (req.values["recipient"].split(";") if "recipient" in
-                    req.values else map(tal_renderer, self.settings['recipient']))
+        from_envelope = (req.values["from_envelope"] or None if "from_envelope" in req.values
+                        else tal_renderer(self.settings["from_envelope"]))
+        from_name = (req.values["from_name"] or None if "from_name" in req.values
+                    else tal_renderer(self.settings['from_name']))
+        from_email = req.values["from_email"] if "from_email" in req.values else tal_renderer(self.settings['from_email'])
+        if "recipient" in req.values:
+            recipient = filter(None, (r.strip() for r in req.values["recipient"].split("\r\n")))
+        else:
+            recipient = map(tal_renderer, self.settings["recipient"])
         subject = req.values["subject"] if "subject" in req.values else tal_renderer(self.settings["subject"])
         text = req.values["text"] if "text" in req.values else tal_renderer(self.settings["text"])
-
+        reply_to_name = (req.values["reply_to_name"] or None if "reply_to_name" in req.values
+                         else tal_renderer(self.settings["reply_to_name"]))
+        reply_to_email = (req.values["reply_to_email"] if "reply_to_email" in
+                        req.values else tal_renderer(self.settings["reply_to_email"]))
         if "sendout" in req.params:
             del req.params["sendout"]
+
             try:
                 self.send_email(
                     node,
-                    mail.EmailAddress(sender, None),
-                    tuple(mail.EmailAddress(r, None) for r in recipient)),
+                    mail.EmailAddress(from_email, from_name),
+                    tuple(mail.EmailAddress(r, None) for r in recipient),
                     subject,
                     text,
+                    from_envelope,
+                    mail.EmailAddress(reply_to_email, reply_to_name) if reply_to_email else None,
                 )
             except:
                 return u'{} &gt;<a href="{}">{}</a>&lt;'.format(
@@ -135,8 +158,12 @@ class WorkflowStep_SendEmail(WorkflowStep):
             return _tal.processTAL(
                     dict(
                         page=u"node?id={}&obj={}".format(self.id, node.id),
-                        sender=sender,
-                        recipient=recipient,
+                        from_envelope=from_envelope,
+                        from_name=from_name,
+                        from_email=from_email,
+                        recipients=u"\r\n".join(recipient),
+                        reply_to_name=reply_to_name,
+                        reply_to_email=reply_to_email,
                         text=text,
                         subject=subject,
                         node=node,
@@ -162,7 +189,20 @@ class WorkflowStep_SendEmail(WorkflowStep):
         data = data.to_dict()
         for attr in ("allowedit", "attach_pdf_form"):
             data[attr] = bool(data.get(attr))
+        assert frozenset(data) == frozenset((
+            "allowedit",
+            "attach_pdf_form",
+            "recipient",
+            "from_email",
+            "from_name",
+            "from_envelope",
+            "reply_to_email",
+            "reply_to_name",
+            "subject",
+            "text",
+        ))
         data["recipient"] = filter(None, (s.strip() for s in data["recipient"].split("\r\n")))
-        assert frozenset(data) == frozenset(("allowedit", "attach_pdf_form", "recipient", "sender", "subject", "text"))
+        for attr in ("from_name", "from_envelope", "reply_to_email", "reply_to_name"):
+            data[attr] = data[attr] or None
         self.settings = data
         db.session.commit()

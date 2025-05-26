7022afd269f1c0d47dfe43b28afd58f1f81c0b09
Nikolaus Kolbenschlag <nikolaus.kolbenschlag@tum.de>
2025-05-26 15:17:06 +0200

    Update mediatum dependencies: nixpkgs-25.11pre805949.bdac72d387dc (#3468)
    old:                                new:
    nginx-1.24.0                        nginx-1.28.0
    postgresql-15.3                     postgresql-17.5
    uwsgi-2.0.20                        uwsgi-2.0.20
    acme.sh-3.0.6                       acme.sh-3.1.1
    ffmpeg-5.1.3-bin                    ffmpeg-7.1.1-bin
    graphicsmagick-1.3.39               graphicsmagick-1.3.45
    graphviz-7.1.0                      graphviz-12.2.1
    pdftk-3.3.3                         pdftk-3.3.3
    poppler-utils-23.02.0               poppler-utils-25.05.0
    icu4c-73.1-dev                      icu4c-76.1-dev/
    perl5.36.0-Image-ExifTool-12.62     perl5.40.0-Image-ExifTool-13.25
    
    Due to glibc dependencies of uwsgi, uwsgi can only be build with
    nixpkgs-22.05pre363272.4d600814942 where
    /nix/store/q29bwjibv9gi9n86203s38n0577w09sx-glibc-2.33-117 is used by
    uwsgi.
    
    Newer versions of glibc prevents the start of uwsgi with:
    
      File "./utils/utils.py", line 34, in <module>
        from lxml import etree
    ImportError: /nix/store/6w8g7njm4mck5dmjxws0z1xnrxvl81xa-glibc-2.34-115/lib/libpthread.so.0:
    version `GLIBC_PRIVATE' not found (required by
    /nix/store/hnp72ygssavd526rp7qk3yjylyc2klcw-glibc-2.27/lib/librt.so.1)

--- postgresql.nix/default.nix
+++ postgresql.nix/default.nix
@@ -1,9 +1,9 @@
 { pkgs ? (import ../nixpkgs.nix {}).pkgs
 , lib ? pkgs.lib
-, postgresql_15 ? pkgs.postgresql_15
+, postgresql_17 ? pkgs.postgresql_17
 }:
 
-postgresql_15.overrideAttrs ( oldAttrs: {
+postgresql_17.overrideAttrs ( oldAttrs: {
   postInstall = oldAttrs.postInstall + ''
     install --verbose -D --mode=0644 --no-target-directory "${./unaccent_german_umlauts_special.rules}" "$out/share/postgresql/tsearch_data/unaccent_german_umlauts_special.rules"
   '';
